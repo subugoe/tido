24140a9be5805bfb8274d95864a41d10e27b3451
Nils Windisch <nils@windisch.me>
2020-04-16 08:52:46 +0200

    UPDATE metadata panel with Quasar UI

--- src/components/metadata.vue
+++ src/components/metadata.vue
@@ -1,42 +1,47 @@
 <template>
-  <div :class="[ css.metadata, css.fixed ]">
-    <ul>
-      <li v-if="Object.keys(collection).length" :class="css.item">
-        <div :class="[ css.headingstyle, css.heading ]">
-          Collection
-        </div>
-        <div :class="css.label">
-          Title:
-        </div>
-        <div :class="css.description">
-          {{ collection.title ? collection.title[0].title : '' }}
-        </div>
-        <div :class="css.label">
-          Collector:
-        </div>
-        <div :class="css.description">
-          {{ collection.collector ? collection.collector.name : '' }}
-        </div>
-        <div :class="css.label">
-          Description:
-        </div>
-        <div :class="css.description">
-          {{ collection.description }}
-        </div>
-      </li>
-
-      <li :class="css.item">
-        <div :class="[ css.headingstyle, css.heading ]">
+  <div>
+    <q-list v-if="Object.keys(collection).length">
+      <q-item>
+        <q-item-section class="text-h6">Collection</q-item-section>
+      </q-item>
+      <q-item>
+        <q-item-section>
+          <q-item-label overline>Title:</q-item-label>
+          <q-item-label>
+            {{ collection.title ? collection.title[0].title : '' }}
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item>
+        <q-item-section>
+          <q-item-label overline>Collector:</q-item-label>
+          <q-item-label>
+            {{ collection.collector ? collection.collector.name : '' }}
+            </q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item>
+        <q-item-section>
+          <q-item-label overline>Description:</q-item-label>
+          <q-item-label>
+            {{ collection.description }}
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <q-list>
+      <q-item>
+        <q-item-section class="text-h6">
           Manifest {{ sequenceindex + 1 }} / {{ manifests.length }}
-        </div>
-        <div :class="css.label">
-          Label:
-        </div>
-        <div :class="css.description">
-          {{ manifesttitle }}
-        </div>
-      </li>
-    </ul>
+        </q-item-section>
+      </q-item>
+      <q-item>
+        <q-item-section>
+          <q-item-label overline>Label:</q-item-label>
+          <q-item-label>{{ manifesttitle }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </div>
 </template>
 
@@ -49,15 +54,6 @@ export default {
   },
   data() {
     return {
-      css: {
-        description: 'objectlist-1__descr',
-        fixed: 'objectlist-1--fixed',
-        heading: 'objectlist-1__heading',
-        headingstyle: 'heading-style--4',
-        item: 'objectlist-1__item',
-        label: 'objectlist-1__label',
-        metadata: 'objectlist-1',
-      },
       sequenceindex: 0,
     };
   },
