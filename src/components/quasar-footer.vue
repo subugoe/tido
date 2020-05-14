78ac292e9ef11b0116356935e2acb5315ac02de5
frank-sub <frank.schneider@sub.uni-goettingen.de>
2020-05-14 18:14:10 +0200

    refactor: footer does not belong to the view. move it to main instance and use footer provided by quasar for more flexibility e.g. regarding layoutview

---
+++ src/components/quasar-footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-footer bordered reveal class="bg-white">
+    <Language />
+  </q-footer>
+</template>
+
+<script>
+import Language from '@/components/language.vue';
+
+export default {
+  name: 'Footer',
+  components: {
+    Language,
+  },
+};
+</script>
