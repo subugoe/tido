0ea1d7922143c9b57150bf3b1c0269715a26d9e7
nwindis <windisch@sub.uni-goettingen.de>
2020-10-05 14:07:16 +0200

    FEATURE toggle filter colors
    !15

--- src/components/togglebar/toggleFilter.vue
+++ src/components/togglebar/toggleFilter.vue
@@ -1,14 +1,14 @@
 <template>
+  <!-- FIXME: semantically this is not a section -->
   <section class="filter-buttons">
     <q-btn-dropdown v-if="$q.screen.width < 1100"
-      class="q-mb-md"
-      color="black"
-      label="Toggle panels ..."
+      class="toggle-filter__button q-mb-md"
+      label="Toggle Panels"
       >
       <slot />
     </q-btn-dropdown>
 
-    <div class="q-mb-md without-dropdown" v-else>
+    <div class="q-mb-md toggle-filter__button without-dropdown" v-else>
       <slot />
     </div>
   </section>
@@ -19,5 +19,21 @@
   align-items: center;
   display: flex;
   justify-content: center;
+}
+
+.toggle-filter__button {
+  background-color: $color-toggle-filter__button__background;
+  color: $color-toggle-filter__button__font;
+  @media (prefers-color-scheme: dark) {
+    background-color: $color-toggle-filter__button__background--dark;
+    color: $color-toggle-filter__button__font--dark;
+  }
+
+  &:active,
+  &:hover,
+  &:focus {
+    background-color: $color-toggle-filter__button__background--active;
+    color: $color-toggle-filter__button__font--active;
+  }
 }
 </style>
