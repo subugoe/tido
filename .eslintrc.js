fe639592ba5a7b0bb89abf0ea249eeaf6ab8712c
nwindis <windisch@sub.uni-goettingen.de>
2020-10-19 12:21:38 +0200

    Add prettier
    #91

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
   },
 
   extends: [
-    'airbnb-base',
+    'prettier',
+    'prettier/vue',
     // Uncomment any of the lines below to choose desired strictness,
     // but leave only one uncommented!
     // See https://eslint.vuejs.org/rules/#available-rules
@@ -36,19 +37,6 @@ module.exports = {
 
   // add your custom rules here
   rules: {
-    'no-param-reassign': 'off',
-
-    'import/first': 'off',
-    'import/named': 'error',
-    'import/namespace': 'error',
-    'import/default': 'error',
-    'import/export': 'error',
-    'import/extensions': 'off',
-    'import/no-unresolved': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'import/prefer-default-export': 'off',
-    'prefer-promise-reject-errors': 'off',
-
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'max-len': 0
