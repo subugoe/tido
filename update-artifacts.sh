aa6dc805d5070285b8dc12bf21312a0c8accb35d
Mathias Göbel <goebel@sub.uni-goettingen.de>
2020-11-11 11:13:09 +0100

    feature: get distDir from config

--- update-artifacts.sh
+++ update-artifacts.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# get dist dir
+distdir=$(grep distDir quasar.conf.js | cut -d "'" -f2)
+
 # get the latest 'deploy' artifact which contains the former states/environments
 jobs=$(curl --header "PRIVATE-TOKEN: $API_TOKEN" "https://gitlab.gwdg.de/api/v4/projects/10921/jobs?scope=success" | jq 'sort_by(.finished_at) | reverse')
 latest_deploy_job_id=$(echo $jobs | jq '.[] | select(.stage == "deploy").id' | sed -n 1p)
@@ -8,6 +11,6 @@ artifact=$(curl --output old-artifact.zip --header "PRIVATE-TOKEN: $API_TOKEN" "
 # add current data to artifact
 unzip -u old-artifact.zip
 mkdir -p public/${CI_COMMIT_SHORT_SHA}
-cp --remove-destination --recursive dist/spa/* public/${CI_COMMIT_SHORT_SHA}
+cp --remove-destination --recursive ${distdir}/* public/${CI_COMMIT_SHORT_SHA}
 mkdir -p public/${CI_COMMIT_REF_SLUG}
-cp --remove-destination --recursive dist/spa/* public/${CI_COMMIT_REF_SLUG}
+cp --remove-destination --recursive ${distdir}/* public/${CI_COMMIT_REF_SLUG}
