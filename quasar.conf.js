13c5fe6970f86aed474e0b124fd198c3fd19b9cb
schneider210 <frank.schneider@sub.uni-goettingen.de>
2020-11-12 12:20:26 +0100

    fix: path to styles

--- quasar.conf.js
+++ quasar.conf.js
@@ -14,7 +14,7 @@ module.exports = function (ctx) {
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: [
-      '/style.scss'
+      'style.scss'
     ],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
