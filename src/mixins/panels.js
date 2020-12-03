929f3482b04cb0f3d7ce43cc6fd024130a6c2b1e
schneider210 <frank.schneider@sub.uni-goettingen.de>
2020-12-03 16:42:20 +0100

    feat: combine config of viewer and panels

---
+++ src/mixins/panels.js
@@ -0,0 +1,110 @@
+import Content from '@/components/content.vue';
+import Metadata from '@/components/metadata.vue';
+import OpenSeadragon from '@/components/openseadragon.vue';
+import Treeview from '@/components/tree.vue';
+
+import { v4 as uuidv4 } from 'uuid';
+import { panels } from '@/statics/config';
+
+export default {
+  data: () => ({
+    components: {
+      1: {
+        component: Treeview,
+        label: 'Contents',
+      },
+      2: {
+        component: Metadata,
+        label: 'Metadata',
+      },
+      3: {
+        component: OpenSeadragon,
+        label: 'Image',
+      },
+      4: {
+        component: Content,
+        label: 'Text',
+      },
+      5: {
+        component: null,
+        label: 'Annotations',
+      },
+    },
+    panels,
+  }),
+  methods: {
+    findComponent(id) {
+      return {
+        id,
+        ...this.components[id],
+      };
+    },
+
+    setupPanels() {
+      // will change all ids => component
+      this.panels = this.panels.map((item) => {
+        const newConnectors = item.connector.map(
+          (c) => (typeof c === 'number' ? this.findComponent(c) : c),
+        );
+        return {
+          ...item,
+          tab_model: 'tab0',
+          connector: newConnectors,
+        };
+      });
+      return this.panels.filter((p) => p.show);
+    },
+  },
+  mounted() {
+    this.panels = this.setupPanels();
+
+    this.$root.$on('panels-position', (newPanels) => {
+      this.panels = newPanels;
+    });
+
+    this.$root.$on('add-panel', () => {
+      const newPanel = {
+        id: uuidv4(),
+        panel_label: 'New Panel',
+        connector: [],
+        tab_model: 'tab0',
+        show: true,
+      };
+      this.panels.push(newPanel);
+    });
+
+    this.$root.$on('update-panellabel', (payload) => {
+      const value = payload.v || ' ';
+      this.panels[payload.index].panel_label = value;
+    });
+
+    this.$root.$on('handle-connector', (payload) => {
+      const {
+        from,
+        to,
+        idC,
+      } = payload;
+      const component = this.findComponent([idC]);
+
+      if (from !== to) {
+        // Add the component to the new panel
+
+        this.panels[to].connector.push(component);
+
+        // Remove the component from the previous panel
+
+        const fromPanel = this.panels[from];
+
+        fromPanel.connector = fromPanel.connector.filter((c) => c.id !== idC);
+
+        // Check if it is the last component inside the panel, then remove the panel
+
+        if (!fromPanel.connector.length) this.$root.$emit('remove-panel', fromPanel.id);
+      }
+    });
+
+    this.$root.$on('remove-panel', (id) => {
+      this.panels = this.panels.filter((c) => c.id !== id);
+    });
+  },
+};
---
+++ src/statics/config.js
@@ -0,0 +1,93 @@
+/*
+  Configuration options
+  =====================
+  src/statics/config.js
+
+  This file serves as a single point for configuration purposes.
+  Please refer to our README for detailed instructions!
+*/
+import { v4 as uuidv4 } from 'uuid';
+
+const conf = {
+  entrypoint: 'https://ahikar-test.sub.uni-goettingen.de/api/textapi/ahikar/3r9ps/collection.json',
+  colors: {
+    primary: '',
+    secondary: '',
+    accent: '',
+  },
+  headers: {
+    all: true,
+    info: true,
+    navigation: true,
+    panels: true,
+    toggle: true,
+  },
+  labels: {
+    item: 'Sheet',
+    manifest: 'Manuscript',
+  },
+  meta: {
+    collection: {
+      all: true,
+      collector: true,
+      description: true,
+      title: true,
+    },
+    manifest: {
+      all: true,
+      creation: true,
+      editor: true,
+      label: true,
+      location: true,
+      origin: true,
+    },
+    item: {
+      all: true,
+      label: true,
+      language: true,
+    },
+  },
+  standalone: true,
+};
+
+// -- Configuration for the panels --
+
+// each "connector" requires at least one "component id" to get the appropriate panel rendered;
+// providing more than one id turns the panels into tabs.
+
+// -- Component IDs for reference --
+
+// - 1 = Treeview
+// - 2 = Metadata
+// - 3 = OpenSeadragon
+// - 4 = Content / Text
+// - 5 = Annotations
+
+const panels = [
+  {
+    id: uuidv4(),
+    connector: [1, 2],
+    panel_label: 'Tabs',
+    show: true,
+  },
+  {
+    id: uuidv4(),
+    connector: [3],
+    panel_label: 'Image',
+    show: true,
+  },
+  {
+    id: uuidv4(),
+    connector: [4],
+    panel_label: 'Text',
+    show: true,
+  },
+  {
+    id: uuidv4(),
+    connector: [5],
+    panel_label: 'Annotations',
+    show: true,
+  },
+];
+
+export { conf, panels };
