b818667d627cfda042e0843296bbfeb90252870b
schneider210 <frank.schneider@sub.uni-goettingen.de>
2021-03-04 18:07:48 +0100

    feat: provide annotation modifiers. e.g. highlight mode, sorting

--- src/components/annotations.vue
+++ src/components/annotations.vue
@@ -1,32 +1,29 @@
 <template>
-  <div class="q-ma-sm">
-    <div class="sticky q-mb-md">
-      <q-toolbar>
+  <div class="q-ma-sm scroll-panel">
+    <!-- Toggle data types -->
+    <div>
+      <q-toolbar v-if="annotations && annotations.items">
         <q-toolbar-title class="text-uppercase">
           Show / hide data types ( {{ annotations.items.length }} / {{ annotations.partOf.total }} )
         </q-toolbar-title>
       </q-toolbar>
 
       <q-btn-toggle
-        v-model="toggle"
+        v-model="typeModel"
         clearable
-        :options="[
-          { label: 'Names', icon: mdiAccount, value: 'Person' },
-          { label: 'Places', icon: mdiMapMarker, value: 'Place' },
-          { label: 'Comments', icon: mdiComment, value: 'Comment' },
-        ]"
         spread
-        toggle-color="white"
-        toggle-text-color="black"
-        @click="filterTypes(toggle)"
+        :color="$q.dark.isActive ? 'grey-1 text-grey-10' : 'accent'"
+        :options="typeOptions"
+        @click="filterTypes(typeModel)"
       />
     </div>
 
+    <!-- List of Anotations -->
     <div
-      v-if="entities"
-      class="q-mt-md"
+      v-if="items"
+      class="scroll q-mt-lg"
     >
-      <q-toolbar v-if="toggle && items.length">
+      <q-toolbar v-if="items.length">
         <q-toolbar-title class="text-uppercase">
           List of annotations in sheet
         </q-toolbar-title>
@@ -36,11 +33,10 @@
         <q-item
           v-for="annotation in items"
           :key="annotation.id"
-          class="q-pa-sm"
           @click="getAnnotationId(annotation.target.id, annotation.body['x-content-type'])"
         >
           <q-item-section avatar>
-            <q-icon :name="contentTypes[annotation.body['x-content-type']]" />
+            <q-icon :name="types[annotation.body['x-content-type']]" />
           </q-item-section>
 
           <q-item-section>
@@ -53,6 +49,28 @@
           </q-item-section>
         </q-item>
       </q-list>
+    </div>
+
+    <!-- Modifiers -->
+    <div class="absolute-bottom">
+      <div
+        v-for="(modifier, index) in modifiers"
+        :key="index"
+        class="q-pa-md"
+      >
+        <div v-if="items.length > modifier.limit">
+          <span class="text-uppercase">{{ modifier.label }}</span>
+
+          <span class="float-right">
+            <q-btn-toggle
+              v-model="modifier.model"
+              :color="$q.dark.isActive ? 'grey-1 text-grey-10' : 'accent'"
+              :options="modifier.options"
+              size="md"
+            />
+          </span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -70,13 +88,44 @@ export default {
   },
   data() {
     return {
-      contentTypes: {
+      items: [],
+      modifiers: [
+        {
+          label: 'Highlight',
+          limit: 0,
+          model: null,
+          options: [
+            { label: 'All', value: 1 }, { label: 'None', value: 0 },
+          ],
+        },
+        {
+          label: 'Sorting order',
+          limit: 1,
+          model: null,
+          options: [
+            { label: 'Alphabetic', value: 1 }, { label: 'Appearance', value: 0 },
+          ],
+        },
+        {
+          label: 'Sorting direction',
+          limit: 1,
+          model: null,
+          options: [
+            { label: 'Ascending', value: 1 }, { label: 'Descending', value: 0 },
+          ],
+        },
+      ],
+      typeModel: null,
+      typeOptions: [
+        { label: 'Names', icon: mdiAccount, value: 'Person' },
+        { label: 'Places', icon: mdiMapMarker, value: 'Place' },
+        { label: 'Comments', icon: mdiComment, value: 'Comment' },
+      ],
+      types: {
         Comment: mdiComment,
         Person: mdiAccount,
         Place: mdiMapMarker,
       },
-      items: [],
-      toggle: null,
     };
   },
   computed: {
@@ -97,14 +146,10 @@ export default {
       const split = targetId.split('/');
       const id = split[split.length - 1];
 
+      // eslint-disable-next-line no-console
+      console.log(targetId, contentType);
       this.$root.$emit('update-annotation-id', id, contentType);
     },
   },
 };
 </script>
-
-<style lang="scss" scoped>
-  .person {
-    background-color: 'white';
-  }
-</style>
