a55825b19d279c378813617da33fd0a96b190f0e
ManikanthDindigala <dindigala@sub.uni-goettingen.de>
2021-04-07 10:33:14 +0200

    style: added a style class in metadata panel

--- src/components/metadata.vue
+++ src/components/metadata.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="item-content">
     <!-- Collection-->
     <q-list v-if="config.meta.collection.all && Object.keys(collection).length">
       <q-item class="q-px-none">
