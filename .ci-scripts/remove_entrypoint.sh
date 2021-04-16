40b251be8e6888f8e29e35d0eb9d43696d23599e
Michelle Weidling <weidling@sub.uni-goettingen.de>
2021-04-16 14:40:20 +0200

    ci: add variables

--- .ci-scripts/remove_entrypoint.sh
+++ .ci-scripts/remove_entrypoint.sh
@@ -4,6 +4,7 @@ FILE="src/index.template.html"
 
 if grep -q '"entrypoint": ""' "$FILE"; then 
     echo "No entry point update required."
+    echo "CONTINUE_BUILD=true" >> build.env
 else
     echo "Entry point has to be removed."
     mkdir -p /tmp/this && cd /tmp/this || exit
@@ -11,4 +12,5 @@ else
     cd QViewer || exit
     sed -i 's/"entrypoint": ".*"/"entrypoint": ""/' src/index.template.html
     git add src/index.template.html && git commit -m "ci: remove entry point" && git push
+    echo "CONTINUE_BUILD=false" >> build.env
 fi
