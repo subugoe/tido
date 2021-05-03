4f363419470f1bc2a59c85f12a4ca0af04f7baf7
ManikanthDindigala <dindigala@sub.uni-goettingen.de>
2021-05-03 10:15:47 +0200

    refactor: remove space character inside span

--- src/components/urls.vue
+++ src/components/urls.vue
@@ -66,7 +66,7 @@ export default {
           // Here the text is divided into 3 parts of array which takes text from previous element(Max Mustermann ( ),
           // link from next element(https://d-nb.info/gnd/1143543866) and than the last ) is skipped.
           if ((urlText[i - 1]?.text || '').endsWith('(') && (urlText[i + 1]?.text || '').endsWith(')') && element.isLink) {
-            filteredOutput.push({ isLink: true, linkTitle: urlText[i - 1]?.text.replace(')', '').replace('(', ''), text: element.text });
+            filteredOutput.push({ isLink: true, linkTitle: urlText[i - 1]?.text.replace(')', '').replace(' (', ''), text: element.text });
 
             i += 1;
           } else if (element.isLink || !(urlText[i + 1]?.isLink && element.text.endsWith('('))) {
