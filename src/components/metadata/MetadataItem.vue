6787089c9b0d3d97a901e305b50a4c6fe88d7a1f
Paul Pestov <pestov@sub.uni-goettingen.de>
2022-02-23 09:51:05 +0100

    feat: render metadata objects recursively as lists

---
+++ src/components/metadata/MetadataItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <q-item-label
+      v-if="keyIsHeadline"
+      overline
+      class="text-uppercase"
+    >
+      {{ $t(item.key) }}
+    </q-item-label>
+    <MetadataLink
+      v-if="isLink()"
+      :url="item.key"
+      :text="item.value"
+    />
+    <span v-else>{{ item.value }}</span>
+    <MetadataItem
+      v-for="(childItem, idx) in childItems"
+      :key="idx"
+      :item="childItem"
+    />
+  </div>
+</template>
+
+<script>
+import MetadataLink from 'components/metadata/MetadataLink';
+
+export default {
+  name: 'MetadataItem',
+  components: { MetadataLink },
+  props: {
+    item: {
+      type: Object,
+      default: () => {},
+    },
+    keyIsHeadline: {
+      type: Boolean,
+      default: () => false,
+    },
+  },
+  computed: {
+    childItems() {
+      return this.item.metadata || [];
+    },
+  },
+  methods: {
+    mounted() {
+
+    },
+    isLink() {
+      const regex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/ig;
+      const matches = this.item.key.match(regex);
+
+      return matches !== null;
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
