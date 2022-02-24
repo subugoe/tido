4c1b9bf30408d50aa2dc7f0c88f3c50be8d97f25
pestov <pestov@sub.uni-goettingen.de>
2022-02-24 20:57:11 +0000

    feat: render metadata objects recursively as lists

---
+++ src/components/metadata/MetadataLink.vue
@@ -0,0 +1,43 @@
+<template>
+  <a
+    :href="url"
+    :title="text + '- open in a new tab or window'"
+    rel="noopener noreferrer"
+    target="_blank"
+    class="content__link"
+    :class="$q.dark.isActive ? 'text-white' : 'text-accent'"
+  >
+    <span>{{ text }}</span>
+
+    <q-icon
+      :name="fasExternalLinkAlt"
+      size="12px"
+      class="content__link-icon q-pl-xs"
+    />
+  </a>
+</template>
+
+<script>
+import { fasExternalLinkAlt } from '@quasar/extras/fontawesome-v5';
+
+export default {
+  name: 'MetadataLink',
+  props: {
+    url: {
+      type: String,
+      default: () => '',
+    },
+    text: {
+      type: String,
+      default: () => '',
+    },
+  },
+  computed: {
+    fasExternalLinkAlt: () => fasExternalLinkAlt,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
