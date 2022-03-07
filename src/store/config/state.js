52a0d580ad4ec8ed3058a2ccc88218d5dc406327
ManikanthDindigala <dindigala@sub.uni-goettingen.de>
2022-03-07 09:46:39 +0100

    fix: rename the action states

--- src/store/config/state.js
+++ src/store/config/state.js
@@ -1,4 +1,4 @@
-export default function AnnotationState() {
+export default function ConfigState() {
   return {
     config: {},
     initialized: false,
--- src/store/contents/state.js
+++ src/store/contents/state.js
@@ -1,4 +1,4 @@
-export default function AnnotationState() {
+export default function ContentsState() {
   return {
     collection: {},
     collectionTitle: '',
