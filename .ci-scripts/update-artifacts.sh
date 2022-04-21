d0535711b418c86468a8d3eabc14605e46b9d3ab
Paul Pestov <pestov@sub.uni-goettingen.de>
2022-04-21 14:45:18 +0200

    feat: wip

--- .ci-scripts/update-artifacts.sh
+++ .ci-scripts/update-artifacts.sh
@@ -5,12 +5,15 @@
 
 dist_dir=$(grep distDir quasar.conf.js | cut -d "'" -f2)
 project_id=10921
+url = "https://gitlab.example.com/api/v4/projects/${project_id}/jobs/artifacts/develop/download"
+curl --output old-artifact.zip --header "PRIVATE-TOKEN: $API_TOKEN" "$url"
+unzip -u old-artifact.zip
 
 # get and extract the latest 'deploy' artifact which contains the former states/environments
 #jobs=$(curl --header "PRIVATE-TOKEN: $API_TOKEN" "https://gitlab.gwdg.de/api/v4/projects/${project_id}/jobs?scope=success" | jq 'sort_by(.finished_at) | reverse')
 #latest_deploy_job_id=$(echo $jobs | jq '.[] | select(.stage == "deploy").id' | sed -n 1p)
 #curl --output old-artifact.zip --header "PRIVATE-TOKEN: $API_TOKEN" "https://gitlab.gwdg.de/api/v4/projects/10921/jobs/${latest_deploy_job_id}/artifacts"
-#unzip -u old-artifact.zip
+
 #
 ## add current data to artifact. this only takes place if $CONTINUE_BUILD is set to 'true' or we are on a feature/bugfix/… branch.
 ## the differentiation between the branches is necessary because $CONTINUE_BUILD is only set on 'main' and 'develop'.
