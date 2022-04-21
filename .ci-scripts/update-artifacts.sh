09b5eee2d9ff5be3d8e1c1f8ce854d7d907fbf83
Paul Pestov <pestov@sub.uni-goettingen.de>
2022-04-21 17:42:15 +0200

    feat: wip

--- .ci-scripts/update-artifacts.sh
+++ .ci-scripts/update-artifacts.sh
@@ -5,24 +5,14 @@
 
 dist_dir=$(grep distDir quasar.conf.js | cut -d "'" -f2)
 project_id=10921
-curl --output old-artifact.zip --header "PRIVATE-TOKEN: $API_TOKEN" "https://gitlab.gwdg.de/api/v4/projects/${project_id}/jobs/artifacts/develop/download"
-unzip -u old-artifact.zip
 
-# get and extract the latest 'deploy' artifact which contains the former states/environments
-#jobs=$(curl --header "PRIVATE-TOKEN: $API_TOKEN" "https://gitlab.gwdg.de/api/v4/projects/${project_id}/jobs?scope=success" | jq 'sort_by(.finished_at) | reverse')
-#latest_deploy_job_id=$(echo $jobs | jq '.[] | select(.stage == "deploy").id' | sed -n 1p)
-#curl --output old-artifact.zip --header "PRIVATE-TOKEN: $API_TOKEN" "https://gitlab.gwdg.de/api/v4/projects/10921/jobs/${latest_deploy_job_id}/artifacts"
+# Get latest artifact from develop branch
+curl --output old-artifact-develop.zip --header "PRIVATE-TOKEN: $API_TOKEN" "https://gitlab.gwdg.de/api/v4/projects/${project_id}/jobs/artifacts/develop/download/?job"
+unzip -u old-artifact-develop.zip -d artifact-develop
+cp artifact-develop/public/develop/* public/develop/
 
-#
-## add current data to artifact. this only takes place if $CONTINUE_BUILD is set to 'true' or we are on a feature/bugfix/… branch.
-## the differentiation between the branches is necessary because $CONTINUE_BUILD is only set on 'main' and 'develop'.
-#if [[ $CONTINUE_BUILD == "true" || ($CI_COMMIT_BRANCH != "main" && $CI_COMMIT_BRANCH != "develop") ]]; then
-#    mkdir -p public/{${CI_COMMIT_SHORT_SHA},${CI_COMMIT_REF_SLUG}}
-#    echo public/${CI_COMMIT_SHORT_SHA} public/${CI_COMMIT_REF_SLUG} | xargs -n 1 cp --remove-destination --recursive ${dist_dir}/*
-#fi
-
+# Setup app build and Config Tester for the current branch
 mkdir -p public/${CI_COMMIT_REF_SLUG}/config-tester
-
 cp --recursive --remove-destination ${dist_dir}/* public/${CI_COMMIT_REF_SLUG}/
 cp .gitlab/pages/config-tester/* public/${CI_COMMIT_REF_SLUG}/config-tester/
 
