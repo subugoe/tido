54b80884c1da58eac91cda8fe7a8e228be8d9fbf
Paul Pestov <pestov@sub.uni-goettingen.de>
2022-05-03 10:36:08 +0200

    ci: improve config-tester reusability, add Ahiqar Arabic-Karshuni

---
+++ .gitlab/pages/config-tester/config-tester.js
@@ -0,0 +1,31 @@
+function createConfigTester() {
+  const container = document.getElementById('config-tester');
+
+  if (container) {
+    const template =
+      `<div>
+          <h3>Config Tester:</h3>
+          <div class="config-tester-content">
+            <a class="active" href="ahiqar-syriac.html">Ahiqar Syriac</a>
+            <a class="active" href="ahiqar-arabic-karshuni.html">Ahiqar Arabic-Karshuni</a>
+            <a href="gfl.html">GFL</a>
+          </div>
+        </div>`;
+
+    appendTemplate(template, container);
+  }
+}
+
+function appendTemplate(template, container) {
+  container.appendChild(this.convertToElement(template));
+}
+
+function convertToElement(template) {
+  const wrapper = document.createElement('div');
+  wrapper.innerHTML = template;
+  return wrapper.firstChild;
+}
+
+window.onload = () => {
+  createConfigTester();
+}
