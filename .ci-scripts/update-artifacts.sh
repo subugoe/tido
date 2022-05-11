ce87a677864a5eb68eb13a3cec74be1b1f2f7f71
Paul Pestov <pestov@sub.uni-goettingen.de>
2022-05-11 12:03:28 +0200

    wip

--- .ci-scripts/update-artifacts.sh
+++ .ci-scripts/update-artifacts.sh
@@ -10,6 +10,7 @@ mkdir public
 
 # Create branch artifact
 mkdir -p public/${CI_COMMIT_REF_SLUG}/config-tester
+ls ${dist_dir}
 cp --remove-destination ${dist_dir}/* public/${CI_COMMIT_REF_SLUG}/
 cp .gitlab/pages/config-tester/* public/${CI_COMMIT_REF_SLUG}/config-tester/
 
