744035c303ef600807ce379002df7bd79eb92c7f
Paul Pestov <pestov@sub.uni-goettingen.de>
2022-05-11 10:42:30 +0200

    wip

--- .ci-scripts/update-artifacts.sh
+++ .ci-scripts/update-artifacts.sh
@@ -10,19 +10,19 @@ mkdir public
 
 # Create branch artifact
 mkdir -p public/${CI_COMMIT_REF_SLUG}/config-tester
-cp --recursive --remove-destination ${dist_dir}/* public/${CI_COMMIT_REF_SLUG}/
-cp --recursive .gitlab/pages/config-tester/* public/${CI_COMMIT_REF_SLUG}/config-tester/
+cp --remove-destination ${dist_dir}/* public/${CI_COMMIT_REF_SLUG}/
+cp .gitlab/pages/config-tester/* public/${CI_COMMIT_REF_SLUG}/config-tester/
 
 # Create main artifact
 curl --output old-artifact-main.zip --header "PRIVATE-TOKEN: $API_TOKEN" "https://gitlab.gwdg.de/api/v4/projects/${project_id}/jobs/artifacts/main/download?job=pages"
 unzip -u old-artifact-main.zip -d artifact-main
 mkdir -p public/main/config-tester
-cp --recursive artifact-main/public/main/* public/main/
-cp --recursive .gitlab/pages/config-tester/* public/main/config-tester/
+cp artifact-main/public/main/* public/main/
+cp .gitlab/pages/config-tester/* public/main/config-tester/
 
 # Create develop artifact
 curl --output old-artifact-develop.zip --header "PRIVATE-TOKEN: $API_TOKEN" "https://gitlab.gwdg.de/api/v4/projects/${project_id}/jobs/artifacts/develop/download?job=pages"
 unzip -u old-artifact-develop.zip -d artifact-develop
 mkdir -p public/develop/config-tester
-cp --recursive artifact-develop/public/develop/* public/develop/
-cp --recursive .gitlab/pages/config-tester/* public/develop/config-tester/
+cp artifact-develop/public/develop/* public/develop/
+cp .gitlab/pages/config-tester/* public/develop/config-tester/
