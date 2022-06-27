5f56563f8f1e5225f382dcc3d4b860be5653c316
pestov <pestov@sub.uni-goettingen.de>
2022-06-27 10:04:14 +0000

    feat: update metadata styles

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -2,14 +2,14 @@
   <div>
     <h1
       v-if="collectionTitle"
-      class="text-h5 text-bold text-uppercase q-mb-none q-mt-xs"
+      class="text-h3 text-bold text-uppercase q-mb-none q-mt-xs"
       :class="$q.dark.isActive ? 'text-light' : 'text-dark'"
     >
       {{ collectionTitle }}
     </h1>
 
     <h2
-      class="text-h6 text-bold text-uppercase  q-mt-none q-mb-none"
+      class="text-h4 text-bold text-uppercase  q-mt-none q-mb-none"
       :class="$q.dark.isActive ? 'text-light' : 'text-dark'"
     >
       <span>{{ manifestTitle }}</span>
