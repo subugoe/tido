7926aa863bf38a8a4841adc81f78023ed185d2e9
Paul Pestov <pestov@sub.uni-goettingen.de>
2022-09-12 14:08:53 +0200

    test: add annotation tests

---
+++ cypress/integration/annotation.test.js
@@ -0,0 +1,75 @@
+describe('Annotation - Multiple Tabs', () => {
+  beforeEach(() => {
+    cy.visit('/#/?itemurl=https://ahikar-dev.sub.uni-goettingen.de/api/textapi/ahikar/arabic-karshuni/3r7vd-130/latest/item.json');
+  });
+  it('Should display first annotation tab', () => {
+    cy
+      .get('.root.panels-target > .item:nth-child(4) .q-tabs__content .q-tab')
+      .first()
+      .should('have.class', 'q-tab--active');
+
+    cy
+      .get('.root.panels-target > .item:nth-child(4) .q-list .q-item')
+      .first()
+      .contains('حيقار');
+  });
+
+  it('Should switch to second annotation tab', () => {
+    cy
+      .get('.root.panels-target > .item:nth-child(4) .q-tabs__content .q-tab')
+      .eq(1)
+      .click()
+      .should('have.class', 'q-tab--active');
+
+    cy
+      .get('.root.panels-target > .item:nth-child(4) .q-list .q-item')
+      .first()
+      .contains('Successful courtier');
+  });
+
+  it('Should stay on first tab when switch item', () => {
+    cy.get('button.next-item').click();
+    cy
+      .get('.root.panels-target > .item:nth-child(4) .q-tabs__content .q-tab')
+      .first()
+      .should('have.class', 'q-tab--active');
+
+    cy
+      .get('.root.panels-target > .item:nth-child(4) .q-list .q-item')
+      .first()
+      .contains('نادان');
+  });
+
+  it('Should stay on second tab when switch item', () => {
+    cy
+      .get('.root.panels-target > .item:nth-child(4) .q-tabs__content .q-tab')
+      .eq(1)
+      .click();
+
+    cy.get('button.next-item').click();
+
+    cy
+      .get('.root.panels-target > .item:nth-child(4) .q-list .q-item')
+      .first()
+      .contains('Successful courtier');
+  });
+
+  it('Should switch to first tab when switch manifest', () => {
+    cy
+      .get('.root.panels-target > .item:nth-child(4) .q-tabs__content .q-tab')
+      .eq(1)
+      .click();
+
+    cy.get('button.previous-item').click();
+
+    cy
+      .get('.root.panels-target > .item:nth-child(4) .q-tabs__content .q-tab')
+      .first()
+      .should('have.class', 'q-tab--active');
+
+    cy
+      .get('.root.panels-target > .item:nth-child(4) .q-list .q-item')
+      .first()
+      .contains('حيقار');
+  });
+});
