03d01100561b9f58393d8d95da95e9d29a8a4ebe
Paul Pestov <pestov@sub.uni-goettingen.de>
2022-11-12 00:14:49 +0100

    ci: moved config-tester to examples folder, deploys to {branch_name}/examples/{example_name}.html

---
+++ examples/config-menu/config-menu.js
@@ -0,0 +1,34 @@
+function createConfigTester() {
+  const container = document.getElementById('config-menu');
+
+  if (container) {
+    const file = window.location.pathname.split('/').pop();
+    const template = `<div>
+          <h3>Config Tester:</h3>
+          <div class="config-menu-content">
+            <a class="${file === 'ahiqar-syriac.html' ? 'active' : ''}" href="../ahiqar-syriac.html">Ahiqar Syriac</a>
+            <a class="${file === 'ahiqar-arabic-karshuni.html' ? 'active' : ''}" href="../ahiqar-arabic-karshuni.html">Ahiqar Arabic-Karshuni</a>
+            <a class="${file === 'gfl.html' ? 'active' : ''}" href="../gfl.html">GFL</a>
+          </div>
+          <div class="config-menu-collapse" onclick="this.parentElement.parentElement.classList.toggle('collapsed')">
+            <span>Show/Hide</span>
+          </div>
+        </div>`;
+
+    appendTemplate(template, container);
+  }
+}
+
+function appendTemplate(template, container) {
+  container.appendChild(this.convertToElement(template));
+}
+
+function convertToElement(template) {
+  const wrapper = document.createElement('div');
+  wrapper.innerHTML = template;
+  return wrapper.firstChild;
+}
+
+window.onload = () => {
+  createConfigTester();
+};
