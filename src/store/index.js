4eb722960d6407833ced29c6840ce0363b04c785
dindigala <dindigala@sub.uni-goettingen.de>
2022-11-23 10:20:09 +0000

    Feat/#vite-config: Migrate from default Quasar cli to use Quasar-Vite cli, implement TIDO instance for Embed into other projects and code refactors.

--- src/store/index.js
+++ src/store/index.js
@@ -5,15 +5,6 @@ import annotations from './annotations';
 import config from './config';
 import contents from './contents';
 
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
-
 export default store((/* { ssrContext } */) => {
   const Store = createStore({
     modules: {
@@ -21,10 +12,6 @@ export default store((/* { ssrContext } */) => {
       config,
       contents,
     },
-
-    // enable strict mode (adds overhead!)
-    // for dev mode and --debug builds only
-    strict: process.env.DEBUGGING,
   });
 
   return Store;
