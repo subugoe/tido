9abd7c502e7cadd665adf9df6a6bf59bee33fd2d
Paul Pestov <10750176+paulpestov@users.noreply.github.com>
2023-03-16 22:02:27 +0100

    feat: add lazy loading for items in tree

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -11,6 +11,7 @@
       :selected-color="$q.dark.isActive ? 'grey' : ''"
       node-key="url"
       @after-show="onAfterShow"
+      @lazy-load="onLazyLoad"
     >
       <template #default-header="{ node }">
         <div :id="node.url" class="row items-center">{{ node.label }}</div>
@@ -22,6 +23,12 @@
 <script>
 import { biChevronRight } from '@quasar/extras/bootstrap-icons';
 import { delay, isElementVisible } from '@/utils';
+import { request } from '@/utils/http';
+
+async function getManifest(url) {
+  const data = await request(url);
+  return data;
+}
 
 export default {
   name: 'Tree',
@@ -88,16 +95,22 @@ export default {
           label: this.collectionTitle,
           selectable: false,
           url: this.collectionTitle,
-          children: this.manifests.map(({ sequence, label: manifestLabel, id: manifestId }, i) => ({
+          children: this.collection.sequence.map(({ label: manifestLabel, id: manifestId }, i) => ({
             label: manifestLabel ?? this.getDefaultManifestLabel(i),
-            sequence,
+            lazy: this.manifest.id !== manifestId,
             url: manifestId,
             selectable: false,
-            children: (Array.isArray(sequence) ? sequence : [sequence]).map(({ id, label: itemLabel }, j) => ({
-              label: itemLabel ?? this.getDefaultItemLabel(j),
-              url: id,
-              parent: manifestId,
-            })),
+            // Prerender item tree elements for the manifest that should be open at initial load
+            // and don't render children on every other manifest. They will be lazy loaded on expand.
+            ...((this.manifest.id === manifestId)
+              ? {
+                children: this.manifest.sequence.map(({ id, label: itemLabel }, j) => ({
+                  label: itemLabel ?? this.getDefaultItemLabel(j),
+                  url: id,
+                  parent: manifestId,
+                })),
+              }
+              : {}),
           }
           )),
         }];
@@ -128,7 +141,18 @@ export default {
       }
 
       this.$nextTick(() => {
+        const node = this.$refs.treeRef.getNodeByKey(manifestId);
+
+        // We need to remove the lazy loading off of this manifest node and insert children manually,
+        // because Quasar does not trigger lazy loading on this.expanded change.
+        node.lazy = false;
+        node.children = this.manifest.sequence.map(({ id, label: itemLabel }, j) => ({
+          label: itemLabel ?? this.getDefaultItemLabel(j),
+          url: id,
+          parent: manifestId,
+        }));
         this.expanded.push(manifestId);
+
         this.selected = this.itemUrl !== '' ? this.itemUrl : sequence[0]?.id;
       });
     },
@@ -138,9 +162,32 @@ export default {
     async onAfterShow() {
       await delay(100);
       const el = document.getElementById(this.itemUrl);
-      if (!isElementVisible(el, this.$refs.containerRef)) {
+      if (el && !isElementVisible(el, this.$refs.containerRef)) {
         this.scrollIntoView(el);
       }
+    },
+    async onLazyLoad({ node, fail, done }) {
+      const { url, children } = node;
+      if (!url) {
+        done(children);
+        return;
+      }
+
+      const manifest = await getManifest(url);
+
+      if (!manifest) {
+        fail();
+        return;
+      }
+
+      const itemNodes = manifest.sequence.map(({ id, label: itemLabel }, j) => ({
+        label: itemLabel ?? this.getDefaultItemLabel(j),
+        url: id,
+        parent: manifest.id,
+      }));
+
+      if (itemNodes) done(itemNodes);
+      else fail();
     },
     onSelectedChange(value) {
       const { treeRef } = this.$refs;
@@ -154,7 +201,7 @@ export default {
       this.$nextTick(async () => {
         await delay(300);
         const el = document.getElementById(this.itemUrl);
-        if (!isElementVisible(el, this.$refs.containerRef)) {
+        if (el && !isElementVisible(el, this.$refs.containerRef)) {
           this.scrollIntoView(el);
         }
         await delay(100);
@@ -284,10 +331,7 @@ export default {
     .q-tree__node-header-content {
       word-break: break-all;
       user-select: none;
-    }
-
-    .q-tree__arrow {
-      margin-right: 4px;
+      margin-left: 4px;
     }
   }
 }
