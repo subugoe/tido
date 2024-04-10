c0dc3bdf313b448e14b11b955d62c373a7e06298
malkja <orlin.malkja@sub.uni-goettingen.de>
2024-04-10 12:27:12 +0200

    test: add new test for bookmarking

--- tests/cypress/e2e/bookmark.cy.js
+++ tests/cypress/e2e/bookmark.cy.js
@@ -141,10 +141,63 @@ describe('Bookmarking - default opening without tido key' , () => {
      .then(() => {
      cy.wait(200)
      .url()
-      .then((value) => decodeURIComponent(value))
-      .should('include', 'tido=m0_i0');
+     .should('include', 'tido=m0_i0');
      });
     
+  })
+
+  it('Should load the first item in gfl', () => {
+    cy
+     .visit(`http://localhost:2222/gfl-local.html`)
+     .then(() => {
+      cy.wait(200)
+      .url()
+      .should('include', 'tido=i0');
+      });
+  })
+});
+
+describe('Bookmarking - change manifest and/or item indices when switching to a new manifest/item', () => {
+  it('Should change the manifest and item indices when switching to a new item in a new manifest', () => {
+    cy.visit(`http://localhost:2222/ahiqar-arabic-karshuni-local.html?tido=m0_i0`)
+    cy
+      .wait(1000)
+      .get('.panels-target > .item:nth-child(1)')
+      .find('.q-tree > .q-tree__node > .q-tree__node-collapsible > .q-tree__children')
+      .children()
+      .eq(20)
+      .click()
+      .wait(1000)
+      .find('> .q-tree__node-collapsible > .q-tree__children')
+      .children()
+      .eq(1)
+      .click()
+      .find('.q-tree__node-header')
+      .should('have.class', 'q-tree__node--selected')
+      .url()
+      .should('contain', 'tido=m20_i1');
+    
+  })
+
+  it('Should change the item index when switching to a new item inside GLF manifest', () => {
+    cy.visit('http://localhost:2222/gfl-local.html')
+    cy 
+      .wait(1000)
+      .get('.panels-target > .item:nth-child(4)')
+      .find('.item-content > .panel-body') //.q-tree__node > .q-tree__node-collapsible > .q-tree__children')
+      .find('.q-tabs > .q-tabs__content').children().eq(1).click().wait(1000)
+      .get('.panels-target > .item:nth-child(4)')
+      .find('.q-tree__node-collapsible > .q-tree__children')
+      .children()
+      .eq(1)
+      .click()
+      .find('.q-tree__node-header')
+      .wait(1000)
+      .should('have.class', 'q-tree__node--selected')
+      .wait(1000)
+      .url()
+      .should('contain', 'tido=i1')
 
   })
+
 });
