640c74894e81b55767cff60afec71a8b60b1277f
malkja <orlin.malkja@sub.uni-goettingen.de>
2024-05-08 15:17:51 +0200

    chore: minor

--- src/components/metadata/Actor.vue
+++ src/components/metadata/Actor.vue
@@ -11,7 +11,7 @@
 import MetadataValue from '@/components/metadata/MetadataValue.vue';
 import { useI18n } from 'vue-i18n';
 
-const { t, locale: i18nLocale } = useI18n();
+const { t } = useI18n();
 
 export interface Props {
   data: Actor[]
