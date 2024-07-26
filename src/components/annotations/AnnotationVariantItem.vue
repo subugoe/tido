671ae74b46c5b65a70e8bd005013c9495cbdeab5
orlinmalkja <54899269+orlinmalkja@users.noreply.github.com>
2024-07-26 11:27:51 +0200

    feat: display a list of variants objects in a new Variant view
    * feat: variants list
    
    * fix: updated the local ahiqar example with and added the variant component
    
    * refactor: remove the 6th component of variants from panels.js
    
    ---------
    
    Co-authored-by: malkja <orlin.malkja@sub.uni-goettingen.de>

---
+++ src/components/annotations/AnnotationVariantItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <div v-for="(variant, i) in annotation.body.value" :key="i"  class="t-items-center t-flex">
+          <span v-if="variant.witness" v-html="variant.witness" class="t-mr-7"/>
+          <span v-else  class="t-mr-7"> - </span>
+          <span v-html="variant.entry"/>
+    </div>
+</template>
+
+
+
+<script setup lang="ts">
+
+export interface Props {
+  annotation: Annotation
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  annotation: () => <Annotation>{},
+})
+
+</script>
+
+
+
+<style scoped>
+
+</style>
