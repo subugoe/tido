1ec66d101c90d6c6866ea2522e4bdd4af6a21858
malkja <orlin.malkja@sub.uni-goettingen.de>
2024-07-26 14:18:51 +0200

    feat: add a box to the witness of the variant item

--- src/components/annotations/AnnotationVariantItem.vue
+++ src/components/annotations/AnnotationVariantItem.vue
@@ -1,7 +1,9 @@
 <template>
-    <div v-for="(variant, i) in annotation.body.value" :key="i"  class="t-items-center t-flex t-justify-around">
-          <span v-if="variant.witness" v-html="variant.witness" class=""/>
-          <span v-else class="" > - </span>
+    <div v-for="(variant, i) in annotation.body.value" :key="i"  class="t-items-center t-flex t-justify-around t-mb-1">
+          <div class="t-rounded t-box-border t-w-150 t-h-8 t-border-2 t-p-1 t-bg-zinc-200">
+            <span v-if="variant.witness" v-html="variant.witness"/>
+            <span v-else  > - </span>
+          </div>
           <span v-html="variant.entry" class=""/>
     </div>
 </template>
