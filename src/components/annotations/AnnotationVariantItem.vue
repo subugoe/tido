612ff2002c58f1a3f9b400cde872ef6b361546d7
malkja <orlin.malkja@sub.uni-goettingen.de>
2024-08-05 14:00:11 +0200

    feat: add border color for each of the different ‘chips’

--- src/components/annotations/AnnotationVariantItem.vue
+++ src/components/annotations/AnnotationVariantItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-for="(variant, i) in annotation.body.value" :key="i"  class="t-items-center t-flex t-mb-1">
-        <div class="t-relative  t-rounded-3xl t-box-border t-w-75 t-h-8 t-border-2 t-p-[2px]">
+        <div class="t-relative  t-rounded-3xl t-box-border t-w-75 t-h-8 t-border-2 t-p-[2px]" :style="{'border-color': getItemColorBasedOnIndex(i)}">
           <span v-if="variant.witness" v-html="variant.witness" class="t-text-sm"/>
           <span v-else class="t-text-sm"> - </span>
         </div>
@@ -11,13 +11,15 @@
 
 
 <script setup lang="ts">
+import { getItemColorBasedOnIndex } from '@/utils/color';
+
 
 export interface Props {
-annotation: Annotation
+  annotation: Annotation
 }
 
 const props = withDefaults(defineProps<Props>(), {
-annotation: () => <Annotation>{},
+  annotation: () => <Annotation>{},
 })
 
 </script>
