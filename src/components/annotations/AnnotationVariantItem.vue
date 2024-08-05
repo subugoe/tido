9ad797c7abe17e25c1e92eb67d78780ab2d79378
malkja <orlin.malkja@sub.uni-goettingen.de>
2024-08-05 13:26:23 +0200

    feat: style better the "chip"

--- src/components/annotations/AnnotationVariantItem.vue
+++ src/components/annotations/AnnotationVariantItem.vue
@@ -1,9 +1,11 @@
 <template>
-    <div v-for="(variant, i) in annotation.body.value" :key="i"  class="t-items-center t-flex">
-          <span v-if="variant.witness" v-html="variant.witness" class="t-mr-7"/>
-          <span v-else  class="t-mr-7"> - </span>
-          <span v-html="variant.entry"/>
-    </div>
+  <div v-for="(variant, i) in annotation.body.value" :key="i"  class="t-items-center t-flex t-mb-1">
+        <div class="t-relative  t-rounded-3xl t-box-border t-w-75 t-h-8 t-border-2 t-p-[2px]">
+          <span v-if="variant.witness" v-html="variant.witness" class="t-text-sm"/>
+          <span v-else class="t-text-sm"> - </span>
+        </div>
+        <span v-html="variant.entry" class="t-absolute t-right-[50%]"/>
+  </div>
 </template>
 
 
@@ -11,11 +13,11 @@
 <script setup lang="ts">
 
 export interface Props {
-  annotation: Annotation
+annotation: Annotation
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  annotation: () => <Annotation>{},
+annotation: () => <Annotation>{},
 })
 
 </script>
