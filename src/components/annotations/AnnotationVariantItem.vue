69ee315f9a1f1e0cc807fc73424f73ffe28c80ea
orlinmalkja <54899269+orlinmalkja@users.noreply.github.com>
2024-08-08 14:46:39 +0200

    feat: add variant item selection/deselection  (#459)
    * feat: style better the "chip"
    
    * feat: add border color for each of the different ‘chips’
    
    * feat: split up the variant items
    
    * feat: select only one variant item
    
    * feat: select a variant item and toggle accordingly
    
    * feat: add the 'witnesses chips' in the text
    
    * feat: remove 'witnesses chips' in other tabs
    
    we select a few variant items -> chips are shown in the text.
    When we switch the annotation tab i.e to 'Motifs' then the chips should not be shown in the text anymore
    
    * feat: remove the witnesses in the text
    
    for each variant item we deselect, we remove the 'witness chip' from the text
    
    * chore: change the icon names in the config
    
    * style: fix linting
    
    * refactor: remove 'isText' from AnnotationVariantItem
    
    * refactor: use watch to initialize 'variantItemsSelection'
    
    * refactor: access 'target' from the text Panel html element
    
    * refactor: keep the same text of witness served from the backend
    
    * style: minor
    
    * refactor: access the witnesses span from the text panel html el
    
    ---------
    
    Co-authored-by: malkja <orlin.malkja@sub.uni-goettingen.de>

--- src/components/annotations/AnnotationVariantItem.vue
+++ src/components/annotations/AnnotationVariantItem.vue
@@ -1,22 +1,128 @@
 <template>
-    <div v-for="(variant, i) in annotation.body.value" :key="i"  class="t-items-center t-flex">
-          <span v-if="variant.witness" v-html="variant.witness" class="t-mr-7"/>
-          <span v-else  class="t-mr-7"> - </span>
-          <span v-html="variant.entry"/>
+  <div
+    v-for="(variant, i) in annotation.body.value"
+    :key="i"
+    class="t-items-center t-flex t-mb-1 t-relative"
+    :class="[
+      't-py-2 t-px-3 t-mb-1 t-rounded-md',
+      { 'hover:t-bg-gray-200 dark:hover:t-bg-gray-600 t-cursor-pointer': !isActive(annotation) },
+      { 't-bg-gray-300 dark:t-bg-gray-600 active': isVariantItemActive(variant.witness)}]"
+    :data-annotation-id="annotation.id"
+    @click="handleClick(variant.witness, i)"
+  >
+    <div
+      class="t-relative  t-rounded-3xl t-box-border t-w-75 t-h-8 t-border-2 t-p-[2px]"
+      :style="{'border-color': getItemColorBasedOnIndex(i)}"
+    >
+      <span
+        v-if="variant.witness"
+        class="t-text-sm"
+        v-html="variant.witness"
+      />
+      <span
+        v-else
+        class="t-text-sm"
+      > - </span>
     </div>
+    <span
+      class="t-absolute t-right-[50%]"
+      v-html="variant.entry"
+    />
+  </div>
 </template>
 
 
 
 <script setup lang="ts">
+import { getItemColorBasedOnIndex } from '@/utils/color';
+import { reactive, watch } from 'vue';
+import * as AnnotationUtils from '@/utils/annotations';
+
 
 export interface Props {
-  annotation: Annotation
+  annotation: Annotation,
+  isActive: (annotation: Annotation) => boolean,
+  toggle: (annotation: Annotation) => void,
 }
 
 const props = withDefaults(defineProps<Props>(), {
   annotation: () => <Annotation>{},
+  isActive: () => true,
+  toggle: () => null,
 })
+
+let variantItemsSelection = reactive({})
+let variantItemsColors = {}
+
+
+watch(() => props.annotation, () => { 
+  props.annotation.body.value.forEach((variantItem) => {
+    const witness = variantItem.witness
+    variantItemsSelection[witness] = false
+  })
+ })
+
+
+
+function handleClick(witness: string, i: number) {
+  // if at least one variant item is selected, then we don't toggle this annotation
+  // for each variant item: we should save a state of selected or not, so that to show the icon or not...
+  const witnessColor = getItemColorBasedOnIndex(i)
+  if (witness in variantItemsColors === false) variantItemsColors[witness] = witnessColor
+
+  if (!isAtLeastOneVariantItemClicked()) {
+    // for the first variant item of each variant object
+    props.toggle(props.annotation)
+    
+  }
+  if ((isOnlyThisVariantActive(witness)) && (isVariantItemActive(witness))) {
+    // when we have only one variant item of a certain variant object selected and then we deselect it -> remove the blue highlight from the text 
+    props.toggle(props.annotation)
+  }
+  // update the state of 'false' or 'true' whether this variant item is selected or not
+  variantItemsSelection[witness] = !variantItemsSelection[witness] 
+
+  const selector = props.annotation.target[0].selector.value
+  if (variantItemsSelection[witness] === true) {
+    AnnotationUtils.addWitness(selector, witness, variantItemsColors)
+  }
+  else {
+    AnnotationUtils.removeWitness(selector, witness)
+  }
+}
+
+function isAtLeastOneVariantItemClicked() {
+  let isClicked = false
+  Object.keys(variantItemsSelection).forEach((witness) => {
+    if (variantItemsSelection[witness] === true) isClicked = true
+  })
+  return isClicked
+}
+
+function isOnlyThisVariantActive(witness) {
+  let isOnlyThisVariantClicked = true
+  Object.keys(variantItemsSelection).forEach((wit) => {
+    if (variantItemsSelection[wit] === true && wit!== witness) isOnlyThisVariantClicked = false
+  })
+  return isOnlyThisVariantClicked
+}
+
+function isVariantItemActive(witness): boolean{
+  return variantItemsSelection[witness] === true
+}
+
+
+function getVariantItemsSelected(): string[] {
+  let variantItemsSelected: string[] = []
+  Object.keys(variantItemsSelection).forEach((wit) => {
+    if (variantItemsSelection[wit] === true) variantItemsSelected.push(wit)
+  })
+
+  return variantItemsSelected
+
+}
+
+
 
 </script>
 
