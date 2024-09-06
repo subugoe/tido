7549a8043c60d93d9179e99a5a03dbec6e29cf51
malkja <orlin.malkja@sub.uni-goettingen.de>
2024-09-06 17:40:08 +0200

    test: add two tests of variants list
    1. Show the variants tab 2. Show a list of variant items

--- tests/cypress/e2e/variants.cy.js
+++ tests/cypress/e2e/variants.cy.js
@@ -1,34 +1,38 @@
-import selectors from './annotation.cy'
+import { commonSelectors, ahiqarSelectors, gflSelectors } from '../support/globals';
 
+const selectors = {
+    ...commonSelectors,
+    ...ahiqarSelectors,
+  }
 
 describe('VariantsAnnotation', () => {
     describe('VariantsList', () => {
-      beforeEach(() => {
-        cy
-          .visit(`/ahiqar-arabic-karshuni-with-variants-local.html?tido=m20_i1_p0.0-1.0-2.0-3.2`)
-          .get('#text-content')
-          .should('be.visible')
-          .get(selectors.list)
-          .should('be.visible')
-          .get(selectors.listItem)
-          .should('be.visible');
-      });
 
-      it('Should display third annotation tab', () => {
-        cy
-          .get(selectors.tab)
-          .eq(2)
-          .should('have.attr', 'data-p-active', 'true');
-  
-      });
-  
-      it('Should show a list of variant items', () => {
+        beforeEach(() => {
+            cy
+            .visit(`/ahiqar-arabic-karshuni-with-variants-local.html?tido=m20_i1_p0.0-1.0-2.0-3.2`)
+        })
+
+        it('Should display third annotation tab', () => {
+            cy  
+                .get(selectors.tab)
+                .children()
+                .eq(2)
+                .parent()
+                .should('have.attr', 'data-p-active', 'true');
         
+            });
+    
+        it('Should show a list of variant items', () => {
+          cy 
+            .get(selectors.list)
+            .should('be.visible')
+            .children()
+            .should("have.length", 11)
+        });
       });
-  
-      
-    });
 
+    
     describe('Witnesses', () => {
 
     })
