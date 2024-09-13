2ac71db6da2249eead987795e91f6bbc1c1664ae
orlinmalkja <54899269+orlinmalkja@users.noreply.github.com>
2024-09-13 10:38:37 +0200

    test: add (de)select witnesses in drop down test (#467)
    * test: add witness (de)selection in the drop down
    
    * test: change the comment for a test
    
    * test: write the test reselection of the witness drop down item in a separate one
    
    ---------
    
    Co-authored-by: malkja <orlin.malkja@sub.uni-goettingen.de>

--- tests/cypress/e2e/variants.cy.js
+++ tests/cypress/e2e/variants.cy.js
@@ -5,6 +5,18 @@ const selectors = {
     ...ahiqarSelectors,
   }
 
+  Cypress.Commands.add('clickWitnessItem', (buttonName, witnessName) => {
+    // click witness of the drop down having button named 'buttonName'
+    cy.get('.panels-wrapper .panel:nth-child(4) .panel-body div#pv_id_6_2_content')
+      .find('button').contains(buttonName)
+      .click()
+      .next()
+      .children()
+      .find('label').contains(witnessName)
+      .prev()
+      .click()
+  })
+
   describe('VariantsAnnotation', () => {
 
     beforeEach(() => {
@@ -72,6 +84,63 @@ const selectors = {
           .invoke('attr', 'data-annotation-level')
           .should('eq', '0')   // highlighted text becomes grey
 
+      })
+    })
+
+    describe('Witnesses', () => {
+      it('Deselects a first witness from the dropdown', () => {
+        cy
+        // click at one target - useful to see how this target's witnesses list change when we unclick at one witness in drop down 
+        .wait(500)
+        .get('div#text-content div#MD12675N1l4l2l6l4l42')
+        .children()
+        .eq(1)
+        .click()
+        
+        // click at the witness 'Cod. Arab. 236' of the drop down 
+        .clickWitnessItem('4 Witnesses selected', 'Cod. Arab. 236')
+
+        // after this part we check the effects of this click
+
+        // 1. remove the witness from the witnesses list of the target+
+        .get('div#text-content div#MD12675N1l4l2l6l4l42')
+        .find('span.witnesses')
+        .children()
+        .should('have.length', 3)
+        .should('not.contain', 'Cod. Arab. 236')
+
+
+        // 2. remove the variant items of this witness from the variants list
+        .get(selectors.list)
+        .children()
+        .eq(3)
+        .invoke('attr', 'data-annotation-id')
+        .should('eq', 'http://ahikar.uni-goettingen.de/ns/annotations/3r14z/annotation-variants-t_Brit_Mus_Add_7209_N1l5l3l5l5l29l4_w_2_1')  // means that first annotation item of this target is DFM 614, instead of Cod Arab
+        .get(selectors.list)
+        .children()
+        .eq(6)         // expecting that two variant items with Cod Arab 236 were removed, the first variant item of the third target is now 'DFM 614'
+        .invoke('attr', 'data-annotation-id')
+        .should('eq', 'http://ahikar.uni-goettingen.de/ns/annotations/3r14z/annotation-variants-t_Brit_Mus_Add_7209_N1l5l3l5l5l29l4_w_3_1')
+      })
+
+      it('Reselect the first witness from the dropdown', () => {
+        cy
+          // unselect and then reselect the witness
+          .clickWitnessItem('4 Witnesses selected', 'Cod. Arab. 236')    // unselect the witness 'Cod. Arab. 236' of the drop down
+          .click()  // reselect the witness
+          .wait(1000)
+
+          // effect: add the variant items of this witness in the variants list
+          .get(selectors.list)
+          .children()
+          .eq(3)
+          .invoke('attr', 'data-annotation-id')
+          .should('eq', 'http://ahikar.uni-goettingen.de/ns/annotations/3r14z/annotation-variants-t_Brit_Mus_Add_7209_N1l5l3l5l5l29l4_w_2_0')  // means that first annotation item of this group is DFM 614, instead of Cod Arab
+          .get(selectors.list)
+          .children()
+          .eq(7)         // expecting that two variant items with Cod Arab 236 were removed, then we aim to access the variant item with witness DFM 614 of the third target with index 6 instead of 8
+          .invoke('attr', 'data-annotation-id')
+          .should('eq', 'http://ahikar.uni-goettingen.de/ns/annotations/3r14z/annotation-variants-t_Brit_Mus_Add_7209_N1l5l3l5l5l29l4_w_3_0')
       })
     })
   });
