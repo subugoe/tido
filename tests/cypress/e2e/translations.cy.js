fc824c8c13d10a6ce28d2597a31121de55b85938
malkja <orlin.malkja@sub.uni-goettingen.de>
2024-09-20 19:00:48 +0200

    test: remove the tests for arabic karshuni 'de'

--- tests/cypress/e2e/translations.cy.js
+++ tests/cypress/e2e/translations.cy.js
@@ -10,28 +10,6 @@ describe('Translations', () => {
     describe('Header Nav buttons text configurable', () => {
 
         it('should show the nav buttons text in german', () => {
-          cy.visit('/ahiqar-arabic-karshuni-local-de.html?tido=m0_i0')
-            .get(selectors.nextButton)
-            .contains('Nächste Seite')
-      
-            .get(selectors.prevButton)
-            .contains('Vorheriges Manuskript')
-            
-          cy.visit('/ahiqar-arabic-karshuni-local-de.html?tido=m0_i1')
-            .get(selectors.nextButton)
-            .contains('Nächste Seite')
-        
-            .get(selectors.prevButton)
-            .contains('Vorherige Seite')
-
-          cy.visit('/ahiqar-arabic-karshuni-local-de.html?tido=tido=m0_i80')
-            .get(selectors.nextButton)
-            .contains('Nächstes Manuskript')
-        
-            .get(selectors.prevButton)
-            .contains('Vorherige Seite')
-
-
            /// GFL Projekt
 
           cy.visit('/gfl-local.html?tido=i0')
@@ -49,8 +27,6 @@ describe('Translations', () => {
             .contains('Vorherige Seite')
          })
       })
-
-       
     
         it('should show the nav buttons text in english', () => {
           cy.visit('/ahiqar-arabic-karshuni-local.html?tido=m0_i0')
