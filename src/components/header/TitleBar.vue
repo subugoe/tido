625f681ae60bef2aa317bb6918de3cd388dc8a7f
malkja <orlin.malkja@sub.uni-goettingen.de>
2024-09-20 19:05:26 +0200

    chore: set the 'item' label temporarily in Title bar
    it has to be fixed in later commits

--- src/components/header/TitleBar.vue
+++ src/components/header/TitleBar.vue
@@ -18,7 +18,7 @@
             class="t-px-2 text-gray-500 dark:text-gray-300"
             name="chevronRight"
           />
-          <span v-if="item">{{ getPageLabel(configStore.config) }} {{ item.n }}</span>
+          <span v-if="item">{{ $t('item') }} {{ item.n }}</span>
         </h2>
       </template>
       <template v-else>
@@ -32,7 +32,7 @@
           <span
             v-if="item"
             class="t-align-middle"
-          >{{ getPageLabel(configStore.config) }} {{ item.n }}</span>
+          >{{ $t('item') }} {{ item.n }}</span>
         </h1>
       </template>
     </template>
@@ -51,7 +51,7 @@ import { useConfigStore } from '@/stores/config';
 import { useContentsStore } from '@/stores/contents'
 import BaseIcon from '@/components/base/BaseIcon.vue';
 import { useI18n } from 'vue-i18n';
-import { isAhiqarWebsite } from '@/utils/translations';
+import { areNavButtonsLabelsInConfig } from '@/utils/translations';
 
 export interface Props {
   item: Item
@@ -70,7 +70,8 @@ const collectionTitle = computed<string | null>(() => contentStore.collectionTit
 const manifestTitle = computed<string | undefined>(() => contentStore.manifest?.label  );
 
 function getPageLabel(config): string {
-  return isAhiqarWebsite(config) ? t('sheet') : t('item')
-  
+  return ''
+  //return areNavButtonsLabelsInConfig(config) ? t('sheet') : t('item')
+
 }
 </script>
