9251caea98a8998a30093e41a488045c7b4545f5
malkja <orlin.malkja@sub.uni-goettingen.de>
2024-10-04 16:36:13 +0200

    fix: change 'citation' key in config

--- src/components/metadata/MetadataItem.vue
+++ src/components/metadata/MetadataItem.vue
@@ -16,9 +16,9 @@
       :value="item.value"
     />
     <Citation 
-      v-if="isCitation(item.key)" 
+      v-if="isCitationRow(item.key) " 
       :value="item.value"
-      />
+    />
     <MetadataItem
       v-for="(childItem, idx) in childItems"
       :key="idx"
@@ -33,7 +33,7 @@ import { computed } from 'vue';
 import MetadataLink from '@/components/metadata/MetadataLink.vue';
 import MetadataValue from '@/components/metadata/MetadataValue.vue';
 import Citation from '@/components/metadata/Citation.vue'
-
+import { useConfigStore } from '@/stores/config';
 
 const props = defineProps<{
   item: Metadata,
@@ -41,6 +41,8 @@ const props = defineProps<{
 
 const label = computed<string>(() => props.item?.key || 'other');
 const childItems = computed<Metadata>(() => props.item?.metadata || []);
+const configStore = useConfigStore()
+
 
 function isLink(): boolean {
   const regex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/ig;
@@ -49,8 +51,33 @@ function isLink(): boolean {
   return matches !== null;
 }
 
-function isCitation(key: string): boolean {
-  return key.toLowerCase() === 'citation' || key.toLowerCase() === 'zitation';
+
+function getMetadataView(panels) {
+  let panelMetadata = panels.filter(panel => panel.label.toLowerCase() === 'metadata');
+  let metadataView;
+  if (panelMetadata.length > 0) {
+    // when metadata is one separate panel
+    metadataView = panelMetadata[0].views[0]
+  }
+  else {
+    // when there is one panel containing content and metadata views
+    panelMetadata = panels.filter(panel => panel.label.toLowerCase() === 'contents_and_metadata')[0]
+    metadataView = panelMetadata.views.filter(view => view.label.toLowerCase() === 'metadata')[0]
+  }
+  return metadataView
+}
+
+function isCitationInConfig(config): boolean {
+  const metadataView = getMetadataView(config.panels)
+  if(metadataView.connector.options?.citation) {
+    return metadataView.connector.options.citation
+  }
+  return false
+}
+
+function isCitationRow(key) {
+  // when we retrieve each MetadataItem, we want to know whether we are in the row of the citation
+  return key.toLowerCase().includes('cit') || key.toLowerCase().includes('zit')
 }
 
 </script>
