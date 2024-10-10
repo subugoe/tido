7483b5fa2b38b05afcaa15e5da2f5fc2b8c77467
malkja <orlin.malkja@sub.uni-goettingen.de>
2024-10-10 12:40:56 +0200

    fix: remove the witness only when there is 'witnesses' html element in text panel

--- src/stores/annotations.ts
+++ src/stores/annotations.ts
@@ -209,13 +209,14 @@ export const useAnnotationsStore = defineStore('annotations', () => {
   }
 
   const resetAnnotations = () => {
+    const textPanelHtml = document.querySelector('#text-content')
     if (annotations.value !== null) {
       annotations.value.forEach((annotation) => {
         const selector = AnnotationUtils.generateTargetSelector(annotation);
         if (selector) {
           AnnotationUtils.highlightTargets(selector, {level: -1});
           AnnotationUtils.removeIcon(annotation);
-          if (AnnotationUtils.isVariant(annotation)) {
+          if (AnnotationUtils.isVariant(annotation) && textPanelHtml.querySelector('.witnesses')) {
             AnnotationUtils.removeWitness(selector, annotation.body.value.witness)
           }
         }
