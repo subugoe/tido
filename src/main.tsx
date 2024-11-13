07cd39766768d19437119d9a3d0caae425ccc256
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2024-11-13 16:33:05 +0100

    chore: import tailwind directives in App

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-
-createRoot(document.getElementById('app')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./css/style.css";
+createRoot(document.getElementById("app")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
+/** @type {import('tailwindcss').Config} */
+
+const config: Config = {
+  darkMode: ["class", '[color-scheme="dark"]'],
+  important: ".tido",
+  prefix: "t-",
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  safelist: [{ pattern: /(text|bg)-(red|blue|primary)+/ }],
+  theme: {
+    extend: {
+      colors: {
+        primary: withOpacity("--color-primary"),
+        "primary-accent": withOpacity("--color-primary-accent"),
+        gray: colors.neutral,
+      },
+    },
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+};
+
+export default config;
