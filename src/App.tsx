f753b54899a3586fa308a60c3b9aa5eece74acb2
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2024-11-13 16:54:34 +0100

    chore: wrap the app component with 'tido' class

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import TopBar from './components/TopBar/TopBar';
-import { FC, useState } from 'react';
-import defaultConfig from './config';
-import { ConfigContext } from './contexts/ConfigContext';
+import TopBar from "./components/TopBar/TopBar";
+import { FC, useState } from "react";
+import defaultConfig from "./config";
+import { ConfigContext } from "./contexts/ConfigContext";
 
 function App() {
-    const [config, setConfig] = useState(defaultConfig);
+  const [config, setConfig] = useState(defaultConfig);
 
-    return (
-        <>
-            <ConfigContext.Provider
-                value={{
-                    config,
-                    setConfig,
-                }}
-            >
-                <TopBar />
-                <h1>Hi!</h1>
-            </ConfigContext.Provider>
-        </>
-    );
+  return (
+    <div className="tido">
+      <ConfigContext.Provider
+        value={{
+          config,
+          setConfig,
+        }}
+      >
+        <TopBar />
+        <h1 className="t-text-red-500">Hi!</h1>
+      </ConfigContext.Provider>
+    </div>
+  );
 }
 
 export default App;
