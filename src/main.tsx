4123ced0ce021338a7277ea02730bf40db77862f
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2024-11-13 16:56:15 +0100

    chore: import two other css files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./css/preflight.scss";
 import "./css/style.css";
+import "./css/style.scss";
+
 createRoot(document.getElementById("app")!).render(
   <StrictMode>
     <App />
