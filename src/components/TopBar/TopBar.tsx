a39f6a139567d948c0d17b48668e324e7ae3ade5
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2024-11-22 12:07:22 +0100

    wip: add an overlay on new panel button

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,19 +1,25 @@
-import { FC, useState, useEffect, useContext } from "react";
+import { FC, useState, useEffect, useContext, useRef } from "react";
+import { OverlayPanel } from "primereact/overlaypanel";
 import { ConfigContext } from "@/contexts/ConfigContext";
 import { getUrls } from "@/utils/config";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
 
 const TopBar: FC = () => {
   //const [collectionsData, setCollectionsData] = useState([''])
   const { config, setConfig } = useContext(ConfigContext);
   const [urls, setUrls] = useState(getUrls(config.panels));
   const [labels, setLabels] = useState([]);
+  const [visible, setVisible] = useState(false);
+  const newButton = useRef(null);
+  const [value, setValue] = useState("");
 
   const manifestLabels =
     labels.length > 0 &&
     labels.map((label, i) => (
       <span
         key={i}
-        className="t-bg-slate-200 t-w-[100px] t-truncate t-p-[5px] t-mr-[20px]"
+        className="t-bg-slate-200 t-w-[200px] t-truncate t-p-[5px] t-mr-[20px]"
         title={label}
       >
         {label}
@@ -48,7 +54,56 @@ const TopBar: FC = () => {
     initManifestsLabels(urls);
   }, []);
 
-  return <div className="t-flex t-mt-[5%] t-ml-[5%]">{manifestLabels}</div>;
+  return (
+    <div className="t-flex t-mt-[5%] t-ml-[5%]">
+      {manifestLabels}
+      <span>
+        <Button
+          icon="pi pi-plus"
+          label="New"
+          pt={{
+            root: {
+              className:
+                "t-bg-blue-500 t-text-white t-rounded t-flex t-items-center t-justify-items-center t-w-[70px] t-h-[34px]",
+            },
+          }}
+          onClick={(e) => {
+            setVisible(true);
+            newButton.current.toggle(e);
+            console.log("new Button", newButton);
+          }}
+        />
+        <OverlayPanel
+          ref={newButton}
+          pt={{
+            root: {
+              className: "t-border-[2px] t-border-slate-500 t-border-solid",
+            },
+            content: { className: "" },
+          }}
+          style={{
+            position: "absolute",
+            borderWidth: "2px",
+            borderColor: "#f3f4f6",
+            borderStyle: "solid",
+            backgroundColor: "white",
+            padding: "5px 10px",
+            borderRadius: 3,
+          }}
+        >
+          <div className="t-flex t-flex-col t-bg-blue-300">
+            <span>Enter a collection/manifest Url</span>
+            <div style={{ marginTop: "10px" }}>
+              <InputText
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+              />
+            </div>
+          </div>
+        </OverlayPanel>
+      </span>
+    </div>
+  );
 };
 
 export default TopBar;
