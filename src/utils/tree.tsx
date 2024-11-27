12b72b9b6343b23e3cb2078816392efecd4dce43
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2024-11-27 17:56:55 +0100

    fix(tree): perform the api calls until the manifest level in order to save time

--- src/utils/tree.tsx
+++ src/utils/tree.tsx
@@ -1,11 +1,8 @@
-let nestingLevel = 0;
-
 export async function getDocumentNode(
   url: string,
   documentType: string,
   key: number
 ) {
-  nestingLevel = 0;
   console.log("get Document node with key", key);
   let node = {};
   const apiData = await fetch(url);
@@ -14,21 +11,22 @@ export async function getDocumentNode(
   node["key"] = key.toString();
   node["label"] = getTitle(data, documentType);
   console.log("creating document node of", documentType);
+  node["nestingLevel"] = 0;
   node["children"] = await getChildrenNodes(node, data.sequence);
   //console.log("document node", node);
   return node;
 }
 
 async function getChildrenNodes(parentNode, items) {
-  nestingLevel += 1;
   // items: 'currentItems'
+  parentNode["nestingLevel"] += 1;
   if (!items || items.length === 0) return [];
   parentNode["children"] = [];
   for (let i = 0; i < items.length; i++) {
     let node = {};
     node["key"] = parentNode.key + "-" + i.toString();
     node["label"] = items[i].label;
-    if (nestingLevel < 2) {
+    if (parentNode["nestingLevel"] < 2) {
       const apiData = await fetch(items[i].id);
       const data = await apiData.json();
       node["children"] = await getChildrenNodes(node, data.sequence);
