1015aa9f8ba3ee0ed1e6f54b67b44d93a478354c
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2024-11-28 14:04:53 +0100

    feat: add ConfigContext in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { FC, useEffect, useState } from "react";
+import defaultConfig from "./config";
+import "primeicons/primeicons.css";
+import { ConfigContext } from "./contexts/ConfigContext";
+
 function App() {
+  const [config, setConfig] = useState(defaultConfig);
+
+  useEffect(() => {}, []);
+
   return (
-    <>
-      <h1>Hi!</h1>
-    </>
-  )
+    <div className="tido">
+      <ConfigContext.Provider
+        value={{
+          config,
+          setConfig,
+        }}
+      ></ConfigContext.Provider>
+    </div>
+  );
 }
 
-export default App
+export default App;
