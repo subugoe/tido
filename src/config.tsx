6cb5a5c77c549144249b0f79fc611cbd93f88fad
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2024-12-04 10:59:31 +0100

    chore: make the config smaller temporarily

--- src/config.tsx
+++ src/config.tsx
@@ -1,30 +1,21 @@
-const defaultConfig = {
+const defaultConfig: Config = {
   globalTree: true,
-  urlConfig: {},
-  labels: {
-    item: 'Sheet',
-    manifest: 'Manuscript',
-  },
-  colors: {
-    forceMode: 'light',
-    primary: '#1a3771',
-  },
   panels: [
     {
-      index: 0,
       collection:
         'https://api.ahiqar.sub.uni-goettingen.de/textapi/ahiqar/syriac/collection.json',
     },
     {
-      index: 1,
       manifest:
         'https://goethes-farbenlehre-berlin.sub.uni-goettingen.de/textapi/Z_1822-02-20_k/manifest.json',
     },
     {
-      index: 2,
       manifest:
         'https://goethes-farbenlehre-berlin.sub.uni-goettingen.de/textapi/ Z_1829-06-28_k/manifest.json',
     },
+    {
+      document: ''
+    }
   ],
 };
 
