5cb2e442c303aad39241240c598118d28b9047a3
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2024-12-04 14:45:54 +0100

    refactor: add two separate functions getManifestUrl() and getCollectionUrl()

--- src/utils/panel.tsx
+++ src/utils/panel.tsx
@@ -1,15 +1,15 @@
 // get the url of the document (collection or manifest) which will be shown in the panel
-export function getPanelUrl(panel: Panel) {
-  if (!('collection' in panel) && !('manifest' in panel)) {
-    console.error('manifest or collection not defined in panel');
-  }
-  if ('collection' in panel) return panel.collection;
-  if ('manifest' in panel) return panel.manifest;
+export function getManifestUrl(panel: Panel): string | null {
+  return panel.manifest ?? null
 }
 
-export function getPanel(url: string, config: Config | undefined): Panel | undefined {
-  if (!config) throw new Error('Config is not defined')
+export function getCollectionUrl(panel: Panel): string | null {
+  return panel.collection ?? null
+}
 
+export function getPanel(url: string | undefined | null, config: Config | undefined): Panel | undefined {
+  if (!config) throw new Error('Config is not defined')
+  if (!url) throw new Error('url is undefined or null')
   return config.panels.find((panel) => {
     if ('collection' in panel) return panel.collection === url
     if ('manifest' in panel) return panel.manifest === url
