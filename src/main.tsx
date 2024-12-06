4f4df05af0454021ba30e88c4f6c336c76cbe84e
Paul Pestov <10750176+paulpestov@users.noreply.github.com>
2024-12-06 17:39:33 +0100

    refactor: remove sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import { ConfigProvider} from '@/contexts/ConfigContext';
 
-import App from './App.tsx';
-import './css/preflight.scss';
+import './css/preflight.css';
 import './css/style.css';
-import './css/style.scss';
 
-
+import App from './App.tsx';
 
 window.Tido = function Tido(config = {}) {
     createRoot(document.getElementById('app')!).render(<ConfigProvider><App customConfig={config} /></ConfigProvider>);
 };
-
 
 
 export default window.Tido;
