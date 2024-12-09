ed5232cd70bd34af10fae432f0e3e2d4f6863de1
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2024-12-09 15:41:39 +0100

    chore: include error handling in get() which fetches api data

--- src/utils/http.tsx
+++ src/utils/http.tsx
@@ -1,6 +1,26 @@
-export async function readApi(url: string) {
-  const apiData = await fetch(url);
-  const data = await apiData.json();
+interface ErrorInterface {
+  error: string
+}
 
-  return data;
+export async function get(url: string): Promise<ErrorInterface | Manifest | Collection> {
+  let response = {}
+  try {
+    const apiData = await fetch(url);
+    if (!apiData.ok) {
+      throw Error('Error while loading document data of this url '+ url)
+    }
+    if (!apiData.headers.get('content-type')?.includes('application/json')) {
+      throw Error('Response from reading this document (collection/manifest) is not a json object')
+    }
+    await apiData.json().then((value) => {
+      response = { ...value }
+     }
+    )
+
+  } catch (e) {
+      response['error'] = e.message
+  }
+  console.log('response', response)
+  return response
+
 }
