cf7768f46687549a92b957552b316593a0cbd0e1
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2024-12-20 17:33:11 +0100

    wip (texts in contentStore): add texts in item of ContentStore

--- src/store/ContentStore.tsx
+++ src/store/ContentStore.tsx
@@ -1,18 +1,38 @@
 import { create } from 'zustand'
+import { request } from '@/utils/http'
 
 interface ContentStoreTypes {
     items: ItemStore[]
     addItemData: (newItemData: Item) => void
+    getItemTexts: (index: number) => string[],
+    addItemTexts: (index: number) => void
 }
 
 export const contentStore = create<ContentStoreTypes>((set, get) => ({
   items: [],
 
   addItemData: (newItemData: Item) => {
-    console.log('items', )
     let newItems = [...get().items]
     newItems.push({item: newItemData})
-    console.log('newItems', newItems)
     set({items: newItems})
-  }
+  },
+
+  getItemTexts: (index: number) => {
+    const content = get().items[index].item.content
+    let texts: string[] = []
+    content.forEach(async (contentItem) => {
+        const response = await request<string>(contentItem.url)
+        if (response.success) {
+            texts.push(response.data) 
+        }
+    })
+
+    return texts
+  },
+
+  addItemTexts: (index: number) => {
+    const texts = get().getItemTexts(index)
+    get().items[index].texts = texts
+  },
+
 }))
