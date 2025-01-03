54a3709af4a8a1ee5188f4c9a1a4120cca2bb1db
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-01-03 10:30:06 +0100

    style: clean code in TextViewOne

--- src/components/panel/central-content/TextViewOne.tsx
+++ src/components/panel/central-content/TextViewOne.tsx
@@ -1,6 +1,5 @@
-import { FC, useState, MouseEvent, useEffect } from 'react';
+import { FC } from 'react';
 
-import { useConfig } from '@/contexts/ConfigContext'
 import CustomHTML from '@/components/CustomHTML';
 
 interface TextViewOneProps {
@@ -8,36 +7,10 @@ interface TextViewOneProps {
 }
 
 const TextViewOne: FC<TextViewOneProps> = ({ textHtml }) => {
-
-const { config } = useConfig()
-
-const [showText, setShowText] = useState(true)
-
-function handleBottomElementClick(e:MouseEvent<HTMLButtonElement>) {
-    e.preventDefault()
-    setShowText(!showText)
-}
-  
-//const imageContainer = <button className="t-w-12 t-h-12 t-ml-[50%]"> <img src={config?.itemImageUrl} /> </button>
-let bottomElement;
-useEffect(() => {
-    function toggleContent() {
-        if (showText) {
-            return <img  />
-        }
-        return  <span> click to show full text</span>
-    }
-    bottomElement = toggleContent()
-}, [showText])
-
-
   return (
-    <div className="">
-        <div style={{display: showText? 'inline': 'none'}}>
-            <CustomHTML textHtml={textHtml} />
+        <div>
+            <CustomHTML textHtml={textHtml} widthText='100' />
         </div>
-       
-    </div>
   );
 };
 
