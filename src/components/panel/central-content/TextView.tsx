e2c7f96c62bfcd5c9f28e573d1556e00f31a0b19
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-01-03 10:28:16 +0100

    style: clean code in TextView

--- src/components/panel/central-content/TextView.tsx
+++ src/components/panel/central-content/TextView.tsx
@@ -8,15 +8,11 @@ interface TextViewOneProps {
 
 const TextViewOne: FC<TextViewOneProps> = ({ textHtml }) => {
 
-
   return (
-    <div className="">
-        <div >
+        <div>
             <CustomHTML textHtml={textHtml} widthText='100%' />
         </div>
-      
-    </div>
-  );
+   );
 };
 
 export default TextViewOne;
