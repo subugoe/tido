34c2e0be7cfa6373373cc1705c42d2e9a3775974
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-01-03 11:13:31 +0100

    style: clean Panel component

--- src/components/panel/Panel.tsx
+++ src/components/panel/Panel.tsx
@@ -1,26 +1,23 @@
-import { FC, useState, useEffect } from 'react'
-
-import { request } from '@/utils/http'
-import { getManifestData, getItemData, isItemContentValid } from '@/utils/panel'
-import { contentStore } from '@/store/ContentStore'
+import { FC, useState } from 'react'
 
 import ContentTypesToggle from '@/components/panel/ContentTypesToggle'
-import ErrorComponent from '@/components/ErrorComponent'
 import PanelCentralContent from '@/components/panel/central-content/PanelCentralContent'
 import PanelTopBar from '@/components/panel/PanelTopBar'
+
+import ErrorComponent from '@/components/ErrorComponent'
 
 interface PanelProps {
   index: number
 }
 
 const Panel: FC<PanelProps> = ({ index }) => {
-  const [error, setError] = useState<boolean | string>(false)
-
+  // TODO: one should get a potential error regarding this panel from probably panels wrapper or a global state of error
+  //        and display the respective error component
+  const [error] = useState<boolean | string>(false)
 
   if (error) {
     return <ErrorComponent message={error} />
   }
-
 
   return (
     <div className="panel t-flex t-flex-col t-w-[600px] t-mr-6 t-border-solid t-border-2 t-border-slate-200 t-rounded-lg t-mt-4 t-px-2.5 t-pt-8 t-pb-6">
