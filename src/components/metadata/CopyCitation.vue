b697eba327cb0411aae6bbf7f352657dd6ee846c
orlinmalkja <54899269+orlinmalkja@users.noreply.github.com>
2025-01-08 15:21:12 +0100

    feat: add a share copy button which copies citation value to clipboard (#530)
    * feat: add a button which copies citation value to clipboard
    
    * style: add citationKey in config and make small code style changes
    
    * fix: show info message on hover and show copied message after clicking the button
    
    * fix: place the tooltip inline with the text and set the message in absolute pos
    
    * chore: restore the icons as they were
    
    * chore: add copy icon and remove some other attributes (like h, w)
    
    * fix: align the icon in the same height as text
    
    * fix: add a new icon
    
    * fix: show the tooltip below the icon
    
    * fix: select the metadata view using conditional filtering on view's connector id
    
    * fix: position the tooltip below the center of the copy button
    
    ---------
    
    Co-authored-by: orlinmalkja <orlin.malkja@sub.uni-goettingen.de>

---
+++ src/components/metadata/CopyCitation.vue
@@ -0,0 +1,67 @@
+<template>
+  <span class="t-ml-1 t-mb-2 t-relative">
+    <Button
+    class="t-h-7"
+    @mouseenter="isHovered = true"
+    @mouseleave="isHovered = false"
+   >
+    <BaseIcon
+      :name="getIconName()"
+      :class="{ 't-text-green-500': copiedCitation, 't-text-gray-400': !copiedCitation}"
+      class="t-text-xl t-align-middle"
+      @click="copyContentToClipboard()"
+    />
+  </Button>
+  <Message
+    v-show="copiedCitation || isHovered"
+    :pt="{
+      root: 't-bg-zinc-700 t-text-white t-text-sm t-absolute t-left-1/2 t--translate-x-1/2 t-w-fit t-whitespace-nowrap t-rounded',
+      wrapper: 't-flex t-flex-row t-pr-1',
+      icon: 't-hidden',
+      text: 't-pl-1',
+      closebutton: {
+        class: 't-hidden',
+      },
+    }"
+  >
+    {{ messageText }}
+  </Message>
+  </span>
+  
+</template>
+
+<script setup lang="ts">
+import Message from "primevue/message";
+import Button from "primevue/button";
+import BaseIcon from "@/components//base/BaseIcon.vue";
+import { ref } from "vue";
+
+const props = defineProps<{
+  value: string;
+}>();
+
+let copiedCitation = ref(false);
+let messageText = ref("Copy citation value");
+const isHovered = ref(false);
+
+async function copyContentToClipboard() {
+  try {
+    await navigator.clipboard.writeText(props.value);
+    copiedCitation.value = true;
+    messageText.value = "Copied!";
+    setTimeout(() => {
+      this.copiedCitation = false;
+      messageText.value = "Copy citation value";
+    }, 1200);
+  } catch (err) {
+    console.error("Citation could not be copied in the keyboard");
+  }
+}
+
+function getIconName() {
+  if (copiedCitation.value === true) {
+    return "check";
+  }
+  return "copy";
+}
+</script>
