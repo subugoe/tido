7f38f75135c742eddc275f44ee5c32300f25ff4c
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-01-31 17:37:05 +0100

    style: minor changes

--- src/components/base/InputField.tsx
+++ src/components/base/InputField.tsx
@@ -6,10 +6,10 @@ interface InputFieldProps {
 
 const InputField: FC<InputFieldProps> = ({ updateInputValue }) => {
 
-    return <>
-        <input className="t-border-solid t-border-[1.5px] t-w-[200px] t-h-[30px] t-mb-[10px]"
-               onChange={(e) => updateInputValue(e.target.value)}/>
-    </>
+  return <>
+    <input className="t-border-solid t-border-[1.5px] t-w-[200px] t-h-[30px] t-mb-[10px]"
+      onChange={(e) => updateInputValue(e.target.value)}/>
+  </>
 }
 
 export default InputField
