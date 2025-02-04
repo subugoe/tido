b9c85fe62d3941ff4f47edb4e4bf7ed03b760f67
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-02-04 14:51:47 +0100

    feat: toggle Global Tree View

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import PanelsWrapper from './components/PanelsWrapper'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { configStore } from '@/store/ConfigStore.tsx'
 
 import TopBar from '@/components/TopBar'
 import GlobalTree from '@/components/tree/GlobalTree.tsx'
 
 interface AppProps {
-    customConfig: Config
+  customConfig: Config
 }
 
 const App: FC<AppProps> = ({ customConfig }) => {
   const addCustomConfig = configStore((state) => state.addCustomConfig)
   addCustomConfig(customConfig)
 
+  const [showGlobalTree, setShowGlobalTree] = useState(customConfig.globalTree)
+
   return (
     <div className="tido t-flex t-flex-col">
-      <TopBar/>
+      <TopBar setShowGlobalTree={setShowGlobalTree}/>
       <div className="t-flex">
-        <GlobalTree/>
+        {showGlobalTree && <GlobalTree/>}
         <PanelsWrapper/>
       </div>
     </div>
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,24 +1,42 @@
-import { FC, useState } from 'react'
+import { Dispatch, FC, SetStateAction, useState } from 'react'
 
 import IconRenderer from '@/components/base/IconRenderer.tsx'
 import { tree } from '@/utils/icons'
+import { cross } from '@/utils/icons'
 
 import TreeSelectionModal from '@/components/TreeSelectionModal.tsx'
 import TreeSelectionModalContent from '@/components/tree-modal/TreeSelectionModalContent.tsx'
 
-const TopBar: FC = () => {
+
+interface TopBarProps {
+  setShowGlobalTree: Dispatch<SetStateAction<boolean | undefined>>
+}
+
+const TopBar: FC<TopBarProps> = ({ setShowGlobalTree }) => {
 
   const [iconHtmlString, setIconHtmlString] = useState(tree)
 
+  function toggleIcon() {
+    if (iconHtmlString === tree) {
+      setIconHtmlString(cross)
+      setShowGlobalTree(true)
+    } else if (iconHtmlString === cross) {
+      setIconHtmlString(tree)
+      setShowGlobalTree(false)
+    }
+  }
+
   const addButton =
-        <span
-          className="t-bg-blue-500 t-text-white t-rounded t-flex t-pl-4 t-items-center t-justify-items-center t-w-16 t-h-10">
+    <span
+      className="t-bg-blue-500 t-text-white t-rounded t-flex t-pl-4 t-items-center t-justify-items-center t-w-16 t-h-10">
             New
-        </span>
+    </span>
 
 
   return <div className="t-flex t-flex-row t-ml-[6%] t-mt-10">
-    <button className="t-mr-2"><IconRenderer htmlString={tree} width={8} height={8}/></button>
+    <button className="t-mr-2" onClick={() => toggleIcon()}><IconRenderer htmlString={iconHtmlString} width={8}
+      height={8}/>
+    </button>
     <TreeSelectionModal TriggerButton={addButton} Content={TreeSelectionModalContent}/>
   </div>
 
