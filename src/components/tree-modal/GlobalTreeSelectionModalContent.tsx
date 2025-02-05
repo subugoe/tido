4644a047edd2a3cbd2b8350e973d1a25115e4643
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-02-05 14:02:01 +0100

    feat: display the update buttons and new button in Global Tree Content Modal

--- src/components/tree-modal/GlobalTreeSelectionModalContent.tsx
+++ src/components/tree-modal/GlobalTreeSelectionModalContent.tsx
@@ -6,11 +6,23 @@ const GlobalTreeSelectionModalContent: FC = () => {
 
   const panels = configStore(state => state.config.panels)
 
+  let buttonsUpdatePanel
+  if (panels && panels?.length > 0) {
+    buttonsUpdatePanel = panels?.map((_, i) => <button
+      className="t-bg-slate-200 t-w-20 t-h-8 t-mr-1 t-rounded-md hover:t-bg-slate-300">Panel {i + 1}</button>)
+  }
 
-  return (<div className="t-bg-blue">
-    Pop over shown
 
-  </div>)
+  return (
+    <div className="t-flex t-items-center t-h-12 t-border-1 t-border-gray-300 t-px-4 t-py-2 t-shadow-md t-rounded-md ">
+      <div className="buttons-update-panel t-flex">
+        {buttonsUpdatePanel}
+      </div>
+      <button className="button-new-panel t-bg-slate-200 t-w-24 t-h-8 t-mr-1 t-rounded-md hover:t-bg-slate-300"> New
+        Panel
+      </button>
+
+    </div>)
 }
 
 export default GlobalTreeSelectionModalContent
