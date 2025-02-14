761019635a23d39f31faa715f645a1d0516c19f1
Paul Pestov <10750176+paulpestov@users.noreply.github.com>
2025-02-14 12:04:52 +0100

    fix: nested metadata display (#570)

--- src/components/metadata/ManifestMetadata.vue
+++ src/components/metadata/ManifestMetadata.vue
@@ -1,9 +1,16 @@
 <template>
-  <div v-if="manifestHasItems" class="manifest-metadata t-mb-7">
+  <div
+    v-if="manifestHasItems"
+    class="manifest-metadata t-mb-7"
+  >
     <h3 class="t-text-xl t-font-semibold t-mb-2">
       {{ $t(labels.manifest) }} {{ number }} / {{ total }}
     </h3>
-    <div v-for="(meta, idx) in metadata" :key="idx" class="t-mb-4">
+    <div
+      v-for="(meta, idx) in metadata"
+      :key="idx"
+      class="t-mb-4"
+    >
       <MetadataItem :item="meta" />
     </div>
     <Actor :data="actor" />
@@ -43,10 +50,7 @@ const metadata = computed(() => {
       key: 'License',
       value: license.id,
     })),
-    ...(manifest.value.metadata || []).map((metaItem) => ({
-      key: metaItem.key,
-      value: metaItem.value,
-    })),
+    ...(manifest.value.metadata || [])
   ];
 
   let orderedMetadata = []
