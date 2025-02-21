fb369081bd4391555595ad364ee18cb923199bb3
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-02-21 14:48:46 +0100

    feat: add support styling

---
+++ src/utils/support-styling.ts
@@ -0,0 +1,38 @@
+import { configStore } from '@/store/ConfigStore.tsx'
+
+function loadFont(url: string, containerSelector: string) {
+  // to use a certain font on a text, then we only need to load the font file.
+  // we load the font file using @font-face interface
+  const root = document.querySelector(`${containerSelector} .tido`)
+  if (!root) return
+
+  const styleEl = document.createElement('style')
+
+  styleEl.id = url
+  styleEl.innerHTML = '@font-face {\n'
+    + `  src: url(${url}) ;\n`
+    + '}'
+
+  root.appendChild(styleEl)
+}
+
+function loadCss (url: string){
+  const element = document.createElement('link')
+
+  element.setAttribute('rel', 'stylesheet')
+  element.setAttribute('type', 'text/css')
+  element.setAttribute('href', url)
+  element.setAttribute('id', url)
+
+  document.head.appendChild(element)
+}
+
+export const getSupport = (support: Support[] | undefined) => {
+  const { container } = configStore.getState().config
+
+  support?.forEach((s) => {
+    const hasElement = document.getElementById(s.url)
+    if (s.type === 'font' && !hasElement) loadFont(s.url, container)
+    if (s.type === 'css' && !hasElement) loadCss(s.url)
+  })
+}
