ca0da19a3b3ee83b6f49a6ca6fa238d58dd8481e
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-02-24 18:59:57 +0100

    wip: show the list of items labels and add a bit of styling

--- src/components/tree/tree-modal/ItemSelectionModalContent.tsx
+++ src/components/tree/tree-modal/ItemSelectionModalContent.tsx
@@ -1,5 +1,4 @@
 import { FC } from 'react'
-import CustomHTML from "@/components/CustomHTML.tsx";
 
 
 interface ItemSelectionModalContentProps {
@@ -8,10 +7,13 @@ interface ItemSelectionModalContentProps {
 
 const ItemSelectionModalContent: FC<ItemSelectionModalContentProps> = ({ itemsList}) => {
 
-  console.log('itemsList', itemsList)
 
-  return <div className="items-list">
-    {itemsList && itemsList.length > 0 &&  itemsList.map((label, i) => <span key={i}> {label} </span>)}
+  return <div>
+    <div className="items-list t-w-auto t-pr-4 t-max-h-80 t-overflow-y-auto">
+      {itemsList && itemsList.length > 0
+        &&  itemsList.map((label, i) =>
+          <div className="t-p-2 hover:t-cursor-pointer hover:t-bg-gray-100 t-rounded-md t-mb-2" key={i}> {label} </div>)}
+    </div>
   </div>
 }
 
