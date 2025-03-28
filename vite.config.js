bc41ca2fa66343b092e2d3935e016342a0bf13c3
Paul Pestov <10750176+paulpestov@users.noreply.github.com>
2025-03-28 11:13:27 +0100

    build: fix merge fail

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,6 @@ import { resolve } from 'path';
 import fs from 'fs';
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      src: resolve(__dirname, './src'),
-      '@': resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 600,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        entryFileNames: 'tido.js',
-        assetFileNames: 'tido.[ext]',
-      },
-    },
-  },
 function injectProjectConfig(projectName) {
   const config = projectName ? fs.readFileSync(`examples/config/${projectName}.json`, 'utf8') : null;
   return {
@@ -32,7 +12,6 @@ function injectProjectConfig(projectName) {
     }
   }
 }
-
 
 export default defineConfig(() => {
   const projectName = process.argv.find(arg => arg.startsWith('--project='))?.split('=')[1] || null;
