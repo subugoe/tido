1256a3e305afb608fabf51e006395f0cd7166963
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-04-17 17:02:14 +0200

    wip: add MetadataItem component

---
+++ src/components/metadata/MetadataItem.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+
+interface MetadataItemProps {
+  item: Metadata
+}
+
+const MetadataItem: FC<MetadataItemProps> = ({ item }) => {
+  const { t } = useTranslation()
+
+  return <div>
+    <div>
+      <h4 className="t-font-semibold t-text-sm t-text-gray-400">
+        {t(item.key)}
+      </h4>
+      <p> {t(item?.value ?? 'value not provided')}</p>
+    </div>
+    <div className="nested-metadata">
+      {
+        item?.metadata?.map((meta, i) => <MetadataItem item={meta} key={i} />)
+      }
+    </div>
+  </div>
+}
+
+export default MetadataItem
