cbcd31c2614de9b062a4f5df3feb537afa49a544
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-04-30 18:09:10 +0200

    wip(tests tree): update a panel using global tree

--- tests/cypress/e2e/tree.cy.js
+++ tests/cypress/e2e/tree.cy.js
@@ -3,10 +3,6 @@ describe('Tree', () => {
     cy.visit('/4w-local.html')
   });
 
-  it('Should render app container', () => {
-    cy.get('[data-cy="app"]').should('be.visible');
-  });
-
   it('Should display collection node', () => {
     cy.get('[data-cy="global-tree-toggle"]').click()
     cy.get('[data-cy="tree-node"]').should('be.visible')
@@ -64,7 +60,32 @@ describe('Tree', () => {
       .children().should('have.length', 2)
       .eq(1)
       .find('[data-cy="item-label"]')
-      .should('have.text', 'Page unknown')
+      .should('have.text', 'Page unknown')     // Label is missing
+    cy.get('[data-cy="global-tree-modal"]').should('not.exist')
+  })
+
+  it('Should update a panel using global tree', () => {
+    cy.get('[data-cy="global-tree-toggle"]').click()
+    cy.get('[data-cy="tree-node"]')
+      .click()
+    cy.get('[data-cy="tree-node-child"]')      // get manifests
+      .first()                                          // click the first manifest
+      .click()
+      .find('[data-cy="tree-node-child"]')
+      .eq(1)                                      // position and click in second item
+      .click()
+
+    cy.get('[data-cy="global-tree-modal"]')
+      .get('[data-cy="buttons-update-panel"]')
+      .children()
+      .eq(0)
+      .click()
+
+    cy.get('[data-cy="panels-wrapper"]')  // check whether the item - 280 -  is opened in first panel
+      .children().should('have.length', 1)
+      .eq(0)
+      .find('[data-cy="item-label"]')
+      .should('have.text', 'Page unknown')      // Label is missing
     cy.get('[data-cy="global-tree-modal"]').should('not.exist')
   })
 });
