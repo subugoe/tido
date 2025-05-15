f3149ce274620d31544081808460574bf3ca138c
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-05-15 16:54:49 +0200

    wip: selection of manifest loads an item

--- src/components/panel/PanelTitle.tsx
+++ src/components/panel/PanelTitle.tsx
@@ -5,6 +5,8 @@ import { Button } from '@/components/ui/button'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover.tsx'
 
 import { useDataStore } from '@/store/DataStore.tsx'
+import { apiRequest } from '@/utils/api.ts'
+import { usePanelStore } from '@/store/PanelStore.tsx'
 
 const PanelHeader: FC = () => {
   const { panelState } = usePanel()
@@ -13,6 +15,7 @@ const PanelHeader: FC = () => {
 
   const collection = useDataStore().collections[panelState.collectionId]?.collection
   const manifestsLabels = collection?.sequence.map((item) => item.label)
+  const updatePanel = usePanelStore(state => state.updatePanel)
 
 
   const handleOpenChange = (open: boolean) => {
@@ -28,12 +31,20 @@ const PanelHeader: FC = () => {
     return 'Page ' + (panelState?.item?.n ?? 'unknown')
   }
 
+  async function handleManifestClick(newManifestLabel: string) {
+    setShowModal(false)
+    const manifestId = collection.sequence.filter((item) => item.label === newManifestLabel)[0].id
+    const manifest = await apiRequest<Manifest>(manifestId)
+    const item = await apiRequest<Item>(manifest.sequence[0].id)
+    updatePanel(panelState.id, { manifest: manifest, item: item })
+  }
+
   return (
     <>
       <div className="flex items-center">
         { (!panelState || !panelState.item) && <Skeleton className="w-[100px] h-6" />  }
-        { panelState && panelState.item &&
-              <Popover>
+        { panelState && panelState.item  &&
+              <Popover onOpenChange={handleOpenChange} modal={true}>
                 <PopoverTrigger asChild>
                   <Button
                     variant={showModal ? 'secondary' : 'ghost'}
@@ -42,11 +53,16 @@ const PanelHeader: FC = () => {
                     { getManifestLabel() }
                   </Button>
                 </PopoverTrigger>
-                <PopoverContent side="bottom" align="start" sideOffset={8} className="w-[350px] h-[400px] overflow-y-auto pr-0 px-4 py-4">
+                {showModal && <PopoverContent side="bottom" align="start" sideOffset={8} className="w-[350px] h-[400px] overflow-y-auto pr-0 px-4 py-4">
                   <div className="flex flex-col space-y-2 text-wrap">
-                    {manifestsLabels.map((label, i) => <Button variant="ghost" key={i} className="text-wrap h-fit overflow-hidden" title={label ?? ''}> {label }</Button>)}
+                    {manifestsLabels.map((label, i) => <Button
+                      variant="ghost"
+                      key={i} className="text-wrap h-fit overflow-hidden"
+                      title={label ?? ''}
+                      onClick={() => handleManifestClick(label)}
+                    > {label }</Button>)}
                   </div>
-                </PopoverContent>
+                </PopoverContent>}
               </Popover> }
         <span className="w-[1px] h-[80%] bg-gray-400 mx-2 grow-0 shrink-0"></span>
         { (!panelState || !panelState.item) && <Skeleton className="w-[40px] h-6" />  }
