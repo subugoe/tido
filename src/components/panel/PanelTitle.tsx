6bdb9ff6ca7f68722654c06c31aeceb0fa676198
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-05-15 16:26:09 +0200

    feat: clicking the manifest label opens a modal with manifests labels

--- src/components/panel/PanelTitle.tsx
+++ src/components/panel/PanelTitle.tsx
@@ -1,9 +1,24 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { usePanel } from '@/contexts/PanelContext.tsx'
 import { Skeleton } from '@/components/ui/skeleton.tsx'
+import { Button } from '@/components/ui/button'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover.tsx'
+
+import { useDataStore } from '@/store/DataStore.tsx'
 
 const PanelHeader: FC = () => {
   const { panelState } = usePanel()
+
+  const [showModal, setShowModal] = useState(false)
+
+  const collection = useDataStore().collections[panelState.collectionId]?.collection
+  const manifestsLabels = collection?.sequence.map((item) => item.label)
+
+
+  const handleOpenChange = (open: boolean) => {
+    setShowModal(open)
+  }
+
 
   function getManifestLabel() {
     return panelState?.manifest?.label ?? null
@@ -17,7 +32,22 @@ const PanelHeader: FC = () => {
     <>
       <div className="flex items-center">
         { (!panelState || !panelState.item) && <Skeleton className="w-[100px] h-6" />  }
-        { panelState && panelState.item && <span className="font-semibold">{ getManifestLabel() }</span>}
+        { panelState && panelState.item &&
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={showModal ? 'secondary' : 'ghost'}
+                    className="font-semibold"
+                    onClick={() => setShowModal(!showModal)}>
+                    { getManifestLabel() }
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent side="bottom" align="start" sideOffset={8} className="w-[350px] h-[400px] overflow-y-auto pr-0 px-4 py-4">
+                  <div className="flex flex-col space-y-2 text-wrap">
+                    {manifestsLabels.map((label, i) => <Button variant="ghost" key={i} className="text-wrap h-fit overflow-hidden" title={label ?? ''}> {label }</Button>)}
+                  </div>
+                </PopoverContent>
+              </Popover> }
         <span className="w-[1px] h-[80%] bg-gray-400 mx-2 grow-0 shrink-0"></span>
         { (!panelState || !panelState.item) && <Skeleton className="w-[40px] h-6" />  }
         { panelState && panelState.item && <span className="text-gray-600" data-cy="item-label">{ getItemLabel() }</span>}
