f5ce6e403e8d4b7633838853cc4294d5034cdcc2
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-05-20 18:11:42 +0200

    tests: remove commented lines

--- tests/cypress/e2e/tree.cy.js
+++ tests/cypress/e2e/tree.cy.js
@@ -62,7 +62,7 @@ describe('Tree', () => {
       .should('have.length', 2)
       .eq(1)
       .find('[data-cy="item-label"]')
-      .should('have.text', 'Page 280')     // Label is missing
+      .should('have.text', 'Page 280')
     cy.get('[data-cy="global-tree-modal"]').should('not.exist')
   })
 
@@ -88,7 +88,7 @@ describe('Tree', () => {
       .should('have.length', 1)
       .eq(0)
       .find('[data-cy="item-label"]')
-      .should('have.text', 'Page 280')      // Label is missing
+      .should('have.text', 'Page 280')
     cy.get('[data-cy="global-tree-modal"]').should('not.exist')
   })
 });
