ce778d9ccd7734703d59af65c2f5feb09a50af15
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-05-20 14:56:22 +0200

    fix: add an active text color to selected manifest and item label in respective modals

--- src/components/panel/ItemLabel.tsx
+++ src/components/panel/ItemLabel.tsx
@@ -5,6 +5,7 @@ import { usePanel } from '@/contexts/PanelContext.tsx'
 import { useDataStore } from '@/store/DataStore.tsx'
 import { apiRequest } from '@/utils/api.ts'
 import { usePanelStore } from '@/store/PanelStore.tsx'
+import { useConfigStore } from '@/store/ConfigStore.tsx'
 
 interface ItemLabelProps {
   selectedManifest: Manifest | null
@@ -15,6 +16,7 @@ interface ItemLabelProps {
 
 const ItemLabel: FC<ItemLabelProps> = ({ selectedManifest, updateSelectedManifest, showItemModal, setShowItemModal }) => {
   const { panelState } = usePanel()
+  const primaryColor = useConfigStore().config.theme.primaryColor
   const collection = useDataStore().collections[panelState.collectionId]?.collection
   const manifest = panelState.manifest
   const updatePanel = usePanelStore(state => state.updatePanel)
@@ -99,12 +101,13 @@ const ItemLabel: FC<ItemLabelProps> = ({ selectedManifest, updateSelectedManifes
             { getItemLabel() }
           </Button>
         </PopoverTrigger>
-        {internalOpen && <PopoverContent side="bottom" align="start" sideOffset={8} className="flex flex-col space-y-4 max-w-[200px] w-fit max-h-[450px] h-fit pr-4 pl-2 py-2">
+        {internalOpen && <PopoverContent side="bottom" align="start" sideOffset={8} className="flex flex-col space-y-2 max-w-[200px] w-fit max-h-[450px] h-fit pr-4 pl-2 py-2">
           <div className="text-gray-600 ml-1">Please select an item to open</div>
           <div className="text-wrap">
             <div className="flex flex-col space-y-1 max-h-[350px] overflow-y-auto">
               {labels.length > 0 && labels.map((label, i) => <Button
                 variant="ghost"
+                style={{ color: 'Page '+ label === getItemLabel() ? primaryColor : '' }}
                 key={i} className="text-wrap h-fit min-h-8 overflow-hidden justify-start pl-2 py-1"
                 title={label ?? ''}
                 onClick={() => handleItemClick(label)}
--- src/components/panel/ManifestLabel.tsx
+++ src/components/panel/ManifestLabel.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button.tsx'
 import { useDataStore } from '@/store/DataStore.tsx'
 import { usePanel } from '@/contexts/PanelContext.tsx'
 import { apiRequest } from '@/utils/api.ts'
+import { useConfigStore } from '@/store/ConfigStore.tsx'
 
 interface ItemLabelProps {
   selectedManifest: Manifest | null,
@@ -15,15 +16,16 @@ interface ItemLabelProps {
 
 const ManifestLabel: FC<ItemLabelProps> = ({ selectedManifest, updateSelectedManifest, showManifestModal, setShowManifestModal, setShowItemModal }) => {
   const { panelState } = usePanel()
+  const primaryColor = useConfigStore().config.theme.primaryColor
   const collection = useDataStore().collections[panelState.collectionId]?.collection
   const manifest = panelState.manifest
   const labels = collection?.sequence.map((item) => item.label)
-  const [label, setLabel] = useState('')
+  const [selectedLabel, setSelectedLabel] = useState('')
 
   useEffect(() => {
     function getManifestLabel() {
       const label = selectedManifest ? selectedManifest.label : panelState?.manifest?.label ?? null
-      setLabel(label)
+      setSelectedLabel(label)
     }
 
     getManifestLabel()
@@ -52,21 +54,22 @@ const ManifestLabel: FC<ItemLabelProps> = ({ selectedManifest, updateSelectedMan
             className="relative font-semibold text-gray-600"
             onClick={() =>  setShowManifestModal(!showManifestModal)}
             data-cy="item-label">
-            { label }
+            { selectedLabel }
             { selectedManifest && <div
               className="absolute mb-8 flex items-center justify-center w-4 h-4 border-[1px] border-gray-400 text-yellow-300 text-md font-bold rounded-full">
                 !
             </div> }
           </Button>
         </PopoverTrigger>
-        {showManifestModal && <PopoverContent side="bottom" align="start" sideOffset={8} className="flex flex-col space-y-4 max-w-[350px] w-fit max-h-[450px] h-fit pr-4 pl-2 py-2">
+        {showManifestModal && <PopoverContent side="bottom" align="start" sideOffset={8} className="flex flex-col space-y-2 max-w-[350px] w-fit max-h-[450px] h-fit pr-4 pl-2 py-2">
           <div className="text-gray-600 ml-1">Please select a manifest to open</div>
           <div className="text-wrap">
             <div className="flex flex-col space-y-1 max-h-[350px] overflow-y-auto">
               {labels.map((label, i) => <Button
                 variant="ghost"
                 key={i}
-                className="h-fit min-h-8 justify-start overflow-hidden pl-2"
+                style={{ color: label === selectedLabel ? primaryColor : '' }}
+                className={`h-fit min-h-8 justify-start overflow-hidden pl-2 `}
                 title={label ?? ''}
                 onClick={() => handleManifestClick(label)}
               > {label }</Button>)}
