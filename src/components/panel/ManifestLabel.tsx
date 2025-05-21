de24c06886ec8f3c57cacefbdd25d261ece906cd
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-05-21 10:46:40 +0200

    wip

--- src/components/panel/ManifestLabel.tsx
+++ src/components/panel/ManifestLabel.tsx
@@ -1,10 +1,13 @@
 import { FC, useEffect, useState } from 'react'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover.tsx'
 import { Button } from '@/components/ui/button.tsx'
+import { useTranslation } from 'react-i18next'
+import { CircleAlert } from 'lucide-react'
+
 import { useDataStore } from '@/store/DataStore.tsx'
 import { usePanel } from '@/contexts/PanelContext.tsx'
 import { apiRequest } from '@/utils/api.ts'
-import { useConfigStore } from '@/store/ConfigStore.tsx'
+
 
 interface ItemLabelProps {
   selectedManifest: Manifest | null,
@@ -16,7 +19,7 @@ interface ItemLabelProps {
 
 const ManifestLabel: FC<ItemLabelProps> = ({ selectedManifest, updateSelectedManifest, showManifestModal, setShowManifestModal, setShowItemModal }) => {
   const { panelState } = usePanel()
-  const primaryColor = useConfigStore().config.theme.primaryColor
+  const { t } = useTranslation()
   const collection = useDataStore().collections[panelState.collectionId]?.collection
   const manifest = panelState.manifest
   const labels = collection?.sequence.map((item) => item.label)
@@ -55,21 +58,17 @@ const ManifestLabel: FC<ItemLabelProps> = ({ selectedManifest, updateSelectedMan
             onClick={() =>  setShowManifestModal(!showManifestModal)}
             data-cy="manifest-label">
             { selectedLabel }
-            { selectedManifest && <div
-              className="absolute mb-4 right-1 flex items-center justify-center w-4 h-4 border-[1px] border-gray-400 text-yellow-300 text-md font-bold rounded-full">
-                !
-            </div> }
+            { selectedManifest && <CircleAlert className="absolute mb-4 right-1 text-yellow-300" /> }
           </Button>
         </PopoverTrigger>
         {showManifestModal && <PopoverContent side="bottom" align="start" sideOffset={8} className="flex flex-col space-y-2 max-w-[350px] w-fit max-h-[450px] pr-0 h-fit pl-2 py-2">
-          <div className="text-gray-600 ml-1">Please select a manifest to open</div>
+          <div className="text-gray-600 ml-1">{ t('please_select_a_manifest_to_open') }</div>
           <div className="text-wrap">
             <div className="flex flex-col space-y-1 max-h-[350px] overflow-y-auto">
               {labels.map((label, i) => <Button
                 variant="ghost"
                 key={i}
-                style={{ color: label === selectedLabel ? primaryColor : '' }}
-                className={`h-fit min-h-8 justify-start overflow-hidden pl-2 mr-2`}
+                className={`h-fit min-h-8 justify-start overflow-hidden pl-2 mr-2 ${panelState.manifest.label === label ? 'text-sky-600': ''} `}
                 title={label ?? ''}
                 onClick={() => handleManifestClick(label)}
               > {label }</Button>)}
