7f91801899fc4b3296286867eac4eaa00f2a416a
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-05-30 14:19:00 +0200

    wip (ItemLabel): use Dropdown shadcn component

--- src/components/panel/ItemLabel.tsx
+++ src/components/panel/ItemLabel.tsx
@@ -1,12 +1,16 @@
 import { FC, useEffect, useRef, useState } from 'react'
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover.tsx'
-import { Button } from '@/components/ui/button.tsx'
 import { useTranslation } from 'react-i18next'
 
 import { usePanel } from '@/contexts/PanelContext.tsx'
 import { useDataStore } from '@/store/DataStore.tsx'
 import { apiRequest } from '@/utils/api.ts'
 import { usePanelStore } from '@/store/PanelStore.tsx'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu.tsx'
 
 interface ItemLabelProps {
   selectedManifest: Manifest | null
@@ -84,33 +88,27 @@ const ItemLabel: FC<ItemLabelProps> = ({ selectedManifest, onItemSelect }) => {
 
 
   return (
-    <>
-      <Popover open={showItemModal} onOpenChange={handleOpenChange}>
-        <PopoverTrigger asChild>
-          <Button
-            variant={showItemModal ? 'secondary' : 'ghost'}
-            onClick={() =>  setShowItemModal(!showItemModal)}
-            className="px-2 py-1 h-7 w-24"
-            title={getItemLabel()}
-            data-cy="item-label">
-            <div className="text-sm rounded-md font-semibold truncate">
-              {getItemLabel()}
-            </div>
-          </Button>
-        </PopoverTrigger>
-        {showItemModal && <PopoverContent side="bottom" align="start" sideOffset={8} className="flex flex-col space-y-2 max-w-[200px] w-fit max-h-[450px] h-fit pr-0 pl-0 py-2">
-          <div className="text-wrap text-muted-foreground ml-1 mb-0 pl-1">{ t('please_select_an_item_to_open') }</div>
-          <div className="flex flex-col space-y-1 max-h-[350px] overflow-y-auto pb-1 pt-1 pl-1">
-            {labels.length > 0 && labels.map((label, i) => <Button
-              variant="ghost"
-              key={label + '_'+ i} className={`h-8 overflow-hidden justify-start mr-2 py-1 pl-2 ${panelState.item.n === label ? 'text-primary hover:text-primary': ''}`}
-              title={label ?? ''}
-              onClick={() => handleItemClick(label)}
-            > {label }</Button>)}
-          </div>
-        </PopoverContent>}
-      </Popover>
-    </>
+    <DropdownMenu
+      open={showItemModal}
+      onOpenChange={handleOpenChange}
+    >
+      <DropdownMenuTrigger asChild>
+        <div className="text-sm rounded-md font-semibold truncate hover:cursor-pointer">
+          {getItemLabel()}
+        </div>
+      </DropdownMenuTrigger>
+      {showItemModal && <DropdownMenuContent>
+        <div className="text-muted-foreground px-2 pt-1 mb-2 ">{ t('please_select_an_item_to_open') }</div>
+        {labels.map((label, i) => <DropdownMenuItem
+          key={label + '_'+i}
+          className={`hover:cursor-pointer ${panelState.item.n === label ? 'data-[highlighted]:text-primary text-primary' : ''} `}
+          title={label ?? ''}
+          onClick={() => handleItemClick(label)}
+
+        > { label }
+        </DropdownMenuItem>)}
+      </DropdownMenuContent>}
+    </DropdownMenu>
   )
 }
 
