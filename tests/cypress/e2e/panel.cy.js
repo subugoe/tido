904026f848d9ecf7434bbc6d68ccbb3fca325e38
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-08-07 17:03:38 +0200

    tests(Panel): add should navigate correctly in local tree

---
+++ tests/cypress/e2e/panel.cy.js
@@ -0,0 +1,51 @@
+Cypress.Commands.add('validateLabel', (type, label) => {
+  // type: 'manifest' or 'item'
+  cy.get('#panels-wrapper')
+    .children().eq(0)
+    .find(`[data-cy=${type}-label]`)
+    .contains(label)
+})
+
+Cypress.Commands.add('validateText', (textSelector, content) => {
+  cy.get('#panels-wrapper')
+    .children().eq(0)
+    .find(textSelector)
+    .contains(content)
+})
+
+describe('Panel', () => {
+  beforeEach(() => {
+    cy.visit('/4w-local.html')
+  });
+
+  it('Should navigate correctly in local tree', () => {
+    // click collection title opens a tree with the expanded root collection and
+    cy.get('[data-cy="panels-wrapper"]')
+      .find('[data-cy="panel"]')
+      .eq(0)
+      .find('[data-cy="collection-title"]')
+      .find('span')
+      .contains('Ebene 1: Reproduktion der Dokumente')
+      .click()
+      .get('[data-cy="tree"]')
+      .children().eq(1)
+      .children('[data-cy="tree-node"]')
+      .should('have.length', 1)           // initially we have one root node
+      .children('[data-cy="node-children"]')
+      .children()
+      .should('have.length', 8)           // children exists -> the root node is expanded initially
+
+      // click at manifest and item updates the content correctly
+      .eq(2).click()
+      .find('[data-cy="node-children"]')
+      .first()
+      .children().should('have.length', 7)    // number of items of manifest
+      .eq(1).click()
+      .get('[data-radix-popper-content-wrapper]')     // popover is closed
+      .should('not.exist')
+    cy.validateLabel('manifest', 'München BSB Cgm 627')
+    cy.validateLabel('item', 'Page 243v')
+    cy.validateText('.text-area', 'fol. 243va')
+  })
+
+})
