bd1de827eea5bb2c8adb1fe321e05bb47dbb95ee
paulpestov <10750176+paulpestov@users.noreply.github.com>
2025-08-08 12:55:45 +0200

    feat: add about modal in settings

--- src/components/header/settings/Settings.tsx
+++ src/components/header/settings/Settings.tsx
@@ -3,17 +3,22 @@ import { useConfigStore } from '@/store/ConfigStore.tsx'
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuTrigger
+  DropdownMenuTrigger,
+  DropdownMenuItem
 } from '@/components/ui/dropdown-menu.tsx'
 import { Button } from '@/components/ui/button.tsx'
-import ThemeToggle from '@/components/header/ThemeToggle.tsx'
-import ShowSelectPanelModeToggle from '@/components/header/ShowSelectPanelModeToggle.tsx'
-import { Settings } from 'lucide-react'
-import { DropdownMenuItem } from '@radix-ui/react-dropdown-menu'
+import ThemeToggle from '@/components/header/settings/ThemeToggle.tsx'
+import ShowSelectPanelModeToggle from '@/components/header/settings/ShowSelectPanelModeToggle.tsx'
+import { CircleQuestionMark, Settings } from 'lucide-react'
+import About from '@/components/header/settings/About.tsx'
+import { useTranslation } from 'react-i18next'
 
 
 const SettingsComp: FC = () => {
+  const { t } = useTranslation()
   const [mounted, setMounted] = useState(false)
+  const [showAboutDialog, setShowAboutDialog] = useState(false)
+
   const showThemeToggle = useConfigStore(state => state.config.showThemeToggle)
 
   useEffect(() => {
@@ -35,8 +40,13 @@ const SettingsComp: FC = () => {
           <DropdownMenuItem asChild>
             <ShowSelectPanelModeToggle />
           </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setShowAboutDialog(true)}>
+            <CircleQuestionMark />
+            <span>{t('about_tido')}</span>
+          </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
+      <About show={showAboutDialog} onClose={() => setShowAboutDialog(false)}></About>
     </div>
   )
 }
