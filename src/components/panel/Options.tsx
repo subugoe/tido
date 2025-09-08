8b899f70090e4c1239ecdebf19f8ed5a8188fd13
paulpestov <10750176+paulpestov@users.noreply.github.com>
2025-09-08 13:58:25 +0200

    feat: move panel modes menu to the options dropdown in panel header

---
+++ src/components/panel/Options.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react'
+import { Button } from '@/components/ui/button.tsx'
+import { Ellipsis, Trash2 } from 'lucide-react'
+import { usePanel } from '@/contexts/PanelContext.tsx'
+import {
+  DropdownMenu,
+  DropdownMenuContent, DropdownMenuGroup,
+  DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu.tsx'
+import PanelModeMenu from '@/components/panel/PanelModeMenu.tsx'
+
+
+const Options: FC = () => {
+  const { usePanelTranslation } = usePanel()
+  const { t } = usePanelTranslation()
+  const { remove } = usePanel()
+
+  function removePanel() {
+    remove()
+  }
+
+  return <DropdownMenu>
+    <DropdownMenuTrigger asChild>
+      <Button variant="ghost" size="icon">
+        <Ellipsis />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end" className="w-48">
+      <DropdownMenuLabel>{ t('options') }</DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      <DropdownMenuLabel className="text-xs">{ t('panel_modes') }</DropdownMenuLabel>
+      <DropdownMenuGroup>
+        <PanelModeMenu />
+      </DropdownMenuGroup>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem onSelect={removePanel} className="text-destructive focus:text-destructive focus:bg-destructive/3">
+        <Trash2 className="mr-2 text-destructive" /> { t('remove_panel') }
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+}
+
+export default Options
