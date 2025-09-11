8f00dbccefa78c1ed3a87342e2885e8bad4067e1
paulpestov <10750176+paulpestov@users.noreply.github.com>
2025-09-11 23:55:09 +0200

    fix: reset text warning on content update, don't overwrite initial panel config at navigation

--- src/components/panel/views/TextViewWarning.tsx
+++ src/components/panel/views/TextViewWarning.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button.tsx'
 import { TriangleAlert } from 'lucide-react'
 
 const TextViewWarning: FC = () => {
-  const { textWarning, usePanelTranslation } = usePanel()
+  const { textWarning, setTextWarning, usePanelTranslation } = usePanel()
   const [visible, setVisible] = useState(false)
 
   const { t } = usePanelTranslation()
@@ -17,7 +17,7 @@ const TextViewWarning: FC = () => {
     { visible && <div className="flex bg-amber-100 dark:bg-amber-800 text-amber-700 dark:text-amber-100 items-center p-2 w-full">
       <TriangleAlert size={15} className="shrink-0" />
       <span className="ml-2 mr-3 truncate text-sm" title={textWarning}>{ textWarning }</span>
-      <Button variant="ghostAmber" className="ml-auto" size="sm" onClick={() => setVisible(false)}>{ t('dismiss') }</Button>
+      <Button variant="ghostAmber" className="ml-auto" size="sm" onClick={() => setTextWarning('')}>{ t('dismiss') }</Button>
     </div> }
   </>
 }
