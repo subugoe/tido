098b2fa357d2b0680c4e2d6e7d6294ee22d09d36
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2025-10-28 17:47:16 +0100

    tests(annotations): should initially display the annotation types as selected

--- tests/cypress/e2e/annotations.cy.js
+++ tests/cypress/e2e/annotations.cy.js
@@ -1,16 +1,20 @@
+
+Cypress.Commands.add('getPanel', () => {
+  cy.get('#panels-wrapper').should('be.visible')
+    .find('.panel')
+})
+
 describe('Annotations', () => {
   beforeEach(() => {
     cy.visit('/ahiqar-local.html')
-    cy.get('#panels-wrapper').should('be.visible')
-      .find('.panel')
+    cy.getPanel()
       .find('[data-cy="sidebar-toggle"]')
       .click()
-      .wait(1000)
+      .wait(100)
   });
 
     it('Should check the initial number of annotations and a few values', () => {
-      cy.get('#panels-wrapper').should('be.visible')
-        .find('.panel')
+      cy.getPanel()
         .find('[data-sidebar-container="true"]')
         .find('[data-annotation]')
         .should('have.length', 7)
@@ -19,7 +23,23 @@ describe('Annotations', () => {
         .parents('[data-sidebar-container="true"]')
         .find('[data-annotation]')
         .eq(2).contains('ܣܪܚܐܕܘܡ')
+    })
 
+    it('Should initially display the annotation types as selected', () => {
+      cy.getPanel()
+        .find('[data-cy="annotations-header"]')
+        .find('[data-cy="annotation-types"]')
+        .children()
+        .should('have.length', 2)
+        .eq(0)
+        .should('contain', 'Person')
+        .should('have.attr', 'data-selected', 'true')
+
+        .parents('[data-cy="annotation-types"]')
+        .children()
+        .eq(1)
+        .should('contain', 'Place')
+        .should('have.attr', 'data-selected', 'true')
     })
   }
 )
