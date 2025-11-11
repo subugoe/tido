c054f597aad54e7b8ede102057db2fdeb58cc882
paulpestov <10750176+paulpestov@users.noreply.github.com>
2025-11-11 16:45:29 +0100

    rollback button outline, update swapper instead

--- src/components/panel/Swapper.tsx
+++ src/components/panel/Swapper.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, ReactNode } from 'react'
 import { Image, Text } from 'lucide-react'
 import { Button } from '@/components/ui/button.tsx'
 import { usePanel } from '@/contexts/PanelContext.tsx'
@@ -6,6 +6,19 @@ import { usePanel } from '@/contexts/PanelContext.tsx'
 interface PreviewProps {
   activeMode?: string,
   onChange?: (value: string) => void,
+}
+
+const PreviewButton: FC<{ children?: ReactNode }> = ({ children }) => {
+  return <Button
+    data-cy='preview-image'
+    className="px-2 py-3 h-auto shadow-md bg-background text-primary border-primary hover:border-primary hover:shadow-lg hover:shadow-primary/20 hover:bg-primary/4 hover:text-primary"
+    variant="outline"
+    color="secondary"
+  >
+    <div className="flex flex-col justify-center items-center">
+      { children }
+    </div>
+  </Button>
 }
 
 const Swapper: FC<PreviewProps> = ({ activeMode = 'A', onChange = () => {} }) => {
@@ -20,29 +33,15 @@ const Swapper: FC<PreviewProps> = ({ activeMode = 'A', onChange = () => {} }) =>
     onChange('A')
   }
 
-  const previewA = <Button
-    data-cy='preview-image'
-    className="px-2 py-3 h-auto shadow-md bg-background"
-    variant="outline"
-    color="secondary"
-  >
-    <div className="flex flex-col justify-center items-center">
-      <Image className="size-8 mb-2" />
-      <div>{t('show_image')}</div>
-    </div>
-  </Button>
+  const previewA = <PreviewButton>
+    <Image className="size-8 mb-2" />
+    <div>{t('show_image')}</div>
+  </PreviewButton>
 
-  const previewB = <Button
-    data-cy='preview-text'
-    className="px-2 py-3 h-auto shadow-md bg-background"
-    variant="outline"
-    color="secondary"
-  >
-    <div className="flex flex-col justify-center items-center">
-      <Text className="size-8 mb-2" />
-      <div>{t('show_text')}</div>
-    </div>
-  </Button>
+  const previewB = <PreviewButton>
+    <Text className="size-8 mb-2" />
+    <div>{t('show_text')}</div>
+  </PreviewButton>
 
   return (
     <div className="z-20 absolute p-[1px] bottom-12 right-12 transition-all"
