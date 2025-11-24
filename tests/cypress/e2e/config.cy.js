ae622718ca7d1820027f09648bde54121b75cf87
Jasper Frerichs <jasper.frerichs@sub.uni-goettingen.de>
2025-11-24 17:20:34 +0100

    Extend config tests

--- tests/cypress/e2e/config.cy.js
+++ tests/cypress/e2e/config.cy.js
@@ -12,11 +12,11 @@ function getPanelModeOption(mode) {
 describe('Config', () => {
   runConfigTest('', 'Should apply defaults', () => {
     cy.get('[data-cy="new-panel"]').should('have.css', 'background-color', 'rgb(52, 86, 170)')
-    cy.get('[data-cy="global-tree-toggle"]').should('be.visible', true)
-    cy.get('[data-cy="new-panel"]').should('be.visible', true)
+    cy.get('[data-cy="global-tree-toggle"]').should('be.visible')
+    cy.get('[data-cy="new-panel"]').should('be.visible')
     cy.get('[data-cy="new-panel"]').should('have.text', 'Add New Panel')
     getPanelModeOption('swap').should('have.attr', 'data-selected', 'true')
-    cy.get('[data-cy="panel-placeholder"]').should('be.visible', true)
+    cy.get('[data-cy="panel-placeholder"]').should('be.visible')
   });
 
   // ===== Specific Config Values =====
@@ -24,10 +24,10 @@ describe('Config', () => {
     cy.get('[data-cy="new-panel"]').should('have.css', 'background-color', 'rgb(0, 255, 0)')
   });
   runConfigTest('showGlobalTree=false', 'showGlobalTree false', () => {
-    cy.get('[data-cy="global-tree-toggle"]').should('be.visible', false)
+    cy.get('[data-cy="global-tree-toggle"]').should('not.exist')
   });
   runConfigTest('showAddNewPanelButton=false', 'showAddNewPanelButton false', () => {
-    cy.get('[data-cy="new-panel"]').should('be.visible', false)
+    cy.get('[data-cy="new-panel"]').should('not.exist')
   });
   runConfigTest('lang=de', 'translations: read from default `de` file', () => {
     cy.get('[data-cy="new-panel"]').should('have.text', 'Neues Panel hinzufügen')
@@ -35,7 +35,22 @@ describe('Config', () => {
   runConfigTest('defaultPanelMode=split', 'defaultPanelMode: split', () => {
     getPanelModeOption('split').should('have.attr', 'data-selected', 'true')
   });
-  runConfigTest('panelPlaceholder=false', 'panelPlaceholder: false', () => {
-    cy.get('[data-cy="panel-placeholder"]').should('be.visible', false)
+  runConfigTest('showPanelPlaceholder=true', 'Should show panel placeholder', () => {
+    cy.get('[data-cy="panel-placeholder"]').should('be.visible')
+  });
+  runConfigTest('showPanelPlaceholder=false', 'Should not show panel placeholder', () => {
+    cy.get('[data-cy="panel-placeholder"]').should('not.exist')
+  });
+  runConfigTest('title=4%20Wachen', 'Should apply custom title', () => {
+    cy.get('h1').should('have.text', '4 Wachen')
+  });
+  runConfigTest('container=%23alternativeApp', 'Should append the app to a custom container', () => {
+    cy.get('#alternativeApp [data-cy="app"]').should('exist')
+    cy.get('#app [data-cy="app"]').should('not.exist')
+  });
+  runConfigTest('allowNewCollections=true', 'Should allow adding a panel from new collections', () => {
+    cy.get('[data-cy="header"] [data-cy="new-panel"]').click()
+    cy.contains('From new collection').click()
+    cy.contains('Enter a collection URL')
   });
 });
