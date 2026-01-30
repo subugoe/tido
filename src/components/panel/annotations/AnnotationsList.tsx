83a15d4c410773f2b006d482e57fb72b161c14d4
orlinmalkja <orlin.malkja@sub.uni-goettingen.de>
2026-01-30 16:02:43 +0100

    feat: add scroll to selected annotation on target click

--- src/components/panel/annotations/AnnotationsList.tsx
+++ src/components/panel/annotations/AnnotationsList.tsx
@@ -1,18 +1,27 @@
-import { FC } from 'react'
+import { FC, useEffect, useRef } from 'react'
 import { usePanel } from '@/contexts/PanelContext.tsx'
 
 import Annotation from '@/components/panel/annotations/Annotation.tsx'
 
 import { getFilteredAnnotations } from '@/utils/annotations.ts'
+import { scrollIntoViewIfNeeded } from '@/utils/dom.ts'
 
 
 const AnnotationsList: FC = () => {
 
-  const { matchedAnnotationsMap } = usePanel()
+  const { matchedAnnotationsMap, selectedAnnotation } = usePanel()
   const filteredAnnotations = getFilteredAnnotations(matchedAnnotationsMap)
+  const annotationsListRef = useRef(null)
+
+  useEffect(() => {
+    if (!annotationsListRef.current || !selectedAnnotation) return
+    const scrollableContainer: HTMLElement = annotationsListRef.current.parentElement as HTMLElement
+    const selectedAnnotationEl = (annotationsListRef.current as HTMLElement).querySelector(`div[data-annotation="${selectedAnnotation.id}"]`) as HTMLElement
+    scrollIntoViewIfNeeded(selectedAnnotationEl, scrollableContainer)
+  }, [selectedAnnotation])
 
 
-  if (filteredAnnotations.length > 0) return <div className={`transition-opacity pt-4`}>
+  if (filteredAnnotations.length > 0) return <div ref={annotationsListRef} className={`transition-opacity pt-4`}>
     {filteredAnnotations.map(a => <Annotation
       data={a}
       key={a.id}
