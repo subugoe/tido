b7b0e85f090dc4cfbde23d33229b404faef42927
paulpestov <10750176+paulpestov@users.noreply.github.com>
2026-02-11 13:46:53 +0100

    fix: discover content types for text without config

--- src/components/panel/views/TextViewError.tsx
+++ src/components/panel/views/TextViewError.tsx
@@ -4,14 +4,17 @@ import { usePanel } from '@/contexts/PanelContext.tsx'
 import { FallbackProps } from 'react-error-boundary'
 import { Button } from '@/components/ui/button.tsx'
 
-const TextViewError: FC<FallbackProps> = ({ error, resetErrorBoundary }) => {
+interface Props extends FallbackProps {
+  error: string
+}
+const TextViewError: FC<Props> = ({ error, resetErrorBoundary }) => {
   const { usePanelTranslation } = usePanel()
   const { t } = usePanelTranslation()
 
   return <div className="w-full overflow-hidden flex flex-col items-center m-3 py-3 px-8 bg-red-50 dark:bg-red-300/20 rounded-lg">
     <CircleX className="text-red-500 mt-[15vh]" size="60" />
     <span className="mt-4 font-semibold text-red-900 dark:text-red-50">{ t('text_loading_failed') }</span>
-    <span className="mt-2 text-red-800/60 dark:text-red-50/50 text-center text-sm break-all">{ error.message }</span>
+    <span className="mt-2 text-red-800/60 dark:text-red-50/50 text-center text-sm break-all">{ error }</span>
     <Button variant="destructive" className="mt-8" onClick={() => resetErrorBoundary()}><RotateCcw /> { t('retry') }</Button>
   </div>
 }
