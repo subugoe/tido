53f4862b724f3557c4cf02ba4fc382ebf5376038
paulpestov <10750176+paulpestov@users.noreply.github.com>
2026-04-20 16:29:23 +0200

    fix: add null check to scrolling function

--- src/utils/scroller.ts
+++ src/utils/scroller.ts
@@ -43,6 +43,7 @@ class Scroller {
   }
 
   scrollOtherTexts(contentUrl: string) {
+    if (!this.syncMaps[contentUrl] || this.texts[contentUrl]) return
     const text = this.texts[contentUrl]
     const textRect = text.getBoundingClientRect()
     const scrollTop = text.scrollTop
