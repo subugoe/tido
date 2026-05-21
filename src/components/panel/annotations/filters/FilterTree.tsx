e2c4161c409c109da8e7f74e5fbdf08f8f78206a
orlinmalkja <54899269+orlinmalkja@users.noreply.github.com>
2026-05-21 10:47:14 +0200

    fix: add optional checking in FilterTree

--- src/components/panel/annotations/filters/FilterTree.tsx
+++ src/components/panel/annotations/filters/FilterTree.tsx
@@ -12,15 +12,15 @@ const FilterTree: FC<Props> = ({ nodes, onToggle }) => {
   const { usePanelTranslation } = usePanel()
   const { t } = usePanelTranslation()
 
-  const anyChildren = nodes.some((node) => node.items && node.items.length > 0)
+  const anyChildren = nodes?.some((node) => node.items && node.items.length > 0)
 
-  if (nodes.length === 0) {
+  if (nodes?.length === 0) {
     return <div className="h-16 px-3 text-sm text-muted-foreground flex justify-center items-center">{ t('no_annotation_filters') }</div>
   }
 
   return <div className="-ml-6">
     {
-      nodes.map((node, index) => (
+      nodes?.map((node, index) => (
         <FilterTreeNode
           key={index}
           node={node}
