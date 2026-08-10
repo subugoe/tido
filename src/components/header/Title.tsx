4aa3a0a25940bba4e4933cf65f154e14cfb780ce
paulpestov <10750176+paulpestov@users.noreply.github.com>
2026-08-10 16:57:35 +0200

    fix: prevent truncating the header title on large screens

--- src/components/header/Title.tsx
+++ src/components/header/Title.tsx
@@ -5,7 +5,7 @@ const Title: FC = () => {
   const { title } = useConfig()
 
   return (
-    <h1 className="text-base sm:text-lg font-bold truncate min-w-0 max-w-40 sm:max-w-64">{ title }</h1>
+    <h1 className="text-base sm:text-lg font-bold truncate min-w-0 max-w-40 sm:max-w-64 xl:max-w-none">{ title }</h1>
   )
 }
 
